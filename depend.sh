2775b5633f2c2bc3c2a55249258ce83846f7d72c
ranma <ranma>
2003-10-17 23:01:54 +0000

    Don't generate dependencies on /usr/[^ ]*\.h

--- depend.sh
+++ depend.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 DIR=`dirname $1`
-case "$DIR" in
-	"" | ".")
-		${CC} -M "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o: Makefile@'
-		;;
-	*)
-		${CC} -M "$@" | sed -e "s@^\(.*\)\.o:@$DIR/\1.d $DIR/\1.o: Makefile@"
-		;;
-esac
+
+if [ "$DIR" = "" -o "$DIR" = "." ]; then
+	DIR=""
+else
+	DIR="$DIR/"
+fi
+
+$CC -M $CFLAGS "$1" |
+	sed -n -e "
+		s@^\\(.*\\)\\.o:@$DIR\\1.d $DIR\\1.o: Makefile@
+		s@/usr/[^ ]*\\.h@@g
+		t foo
+		:foo
+		s@^ *\\\\\$@@
+		t
+		p
+		"
