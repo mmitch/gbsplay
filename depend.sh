91496952230bd985529b32f3fbf5fd3fb786c6b8
ranma <ranma>
2003-10-18 21:35:08 +0000

    Depend on depend.sh and subdir.mk

--- depend.sh
+++ depend.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 DIR=`dirname $1`
+SUBMK=""
 
 if [ "$DIR" = "" -o "$DIR" = "." ]; then
 	DIR=""
 else
 	DIR="$DIR/"
+	SUBMK=" ${DIR}subdir.mk"
 fi
 
 $CC -M $CFLAGS "$1" |
 	sed -n -e "
-		s@^\\(.*\\)\\.o:@$DIR\\1.d $DIR\\1.o: Makefile@
+		s@^\\(.*\\)\\.o:@$DIR\\1.d $DIR\\1.o: depend.sh Makefile$SUBMK@
 		s@/usr/[^ ]*\\.h@@g
 		t foo
 		:foo
