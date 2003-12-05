c5b75aad061c1b8d4571f744daa7845d63927a17
mitch <mitch>
2003-12-05 22:22:30 +0000

    add songlength argument auto-tag ogg files

--- contrib/gbs2ogg.sh
+++ contrib/gbs2ogg.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-# $Id: gbs2ogg.sh,v 1.1 2003/10/11 20:18:49 mitch Exp $
+# $Id: gbs2ogg.sh,v 1.2 2003/12/05 22:22:30 mitch Exp $
 #
 # Automatically convert all subsongs from .gbs file to .ogg files.
 # 
 # 2003 (C) by Christian Garbs <mitch@cgarbs.de>
 #
-# ToDo:
-# - ogg-Tags setzen
 
 FILENAME=$1
 
@@ -14,18 +12,37 @@ ENCODER="oggenc -q6 -r"
 PLAY=206
 FADE=3
 
-if [ -z $1 ] ; then
-    echo "usage:  gbs2ogg.sh <gbs-file>"
+if [ -z "$1" ]; then
+    echo "usage:  gbs2ogg.sh <gbs-file> [subsong_seconds]"
     exit 1
+fi
+
+if [ "$2" ]; then
+    PLAY=$2
 fi
 
 FILEBASE=`basename "$FILENAME"`
 FILEBASE=`echo "$FILEBASE"|sed 's/.gbs$//'`
 
 # get subsong count
-SUBSONGS=`gbsinfo $FILENAME | grep ^Subsongs: | sed -e 's/^Subsongs: //' -e 's/\\.$//'`
+# get song info
+../gbsinfo "$FILENAME" | cut -c 17- | sed -e 's/^"//' -e 's/"$//' | (
+    read GBSVERSION
+    read TITLE
+    read AUTHOR
+    read COPYRIGHT
+    read LOAD_ADDR
+    read INIT_ADDR
+    read PLAY_ADDR
+    read STACK_PTR
+    read FILE_SIZE
+    read ROM_SIZE
+    read SUBSONGS
 
-for SUBSONG in `seq 1 $SUBSONGS`; do
-    printf "== converting song %02d/%02d:\n" $SUBSONG $SUBSONGS
-    gbsplay -s -f $FADE -t $PLAY "$FILENAME" $SUBSONG $SUBSONG | $ENCODER - > `printf "%s-%02d.ogg" "$FILEBASE" $SUBSONG`
-done
+    for SUBSONG in `seq 1 $SUBSONGS`; do
+	printf "== converting song %02d/%02d:\n" $SUBSONG $SUBSONGS
+	gbsplay -s -f $FADE -t $PLAY "$FILENAME" $SUBSONG $SUBSONG \
+	    | $ENCODER -N $SUBSONG -t "$TITLE" -a "$AUTHOR" -c "copyright=$COPYRIGHT" -G "Gameboy music" - \
+	    > `printf "%s-%02d.ogg" "$FILEBASE" $SUBSONG`
+    done
+)
