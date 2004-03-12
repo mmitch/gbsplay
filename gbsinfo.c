53929e6d0ea37deac3c09578e22539de8386b0c5
mitch <mitch>
2004-03-12 19:02:15 +0000

    add -h and -V options

--- gbsinfo.c
+++ gbsinfo.c
@@ -1,8 +1,9 @@
-/* $Id: gbsinfo.c,v 1.4 2003/12/12 23:05:51 ranma Exp $
+/* $Id: gbsinfo.c,v 1.5 2004/03/12 19:02:15 mitch Exp $
  *
  * gbsplay is a Gameboy sound player
  *
- * 2003 (C) by Tobias Diedrich <ranma@gmx.at>
+ * 2003-2004 (C) by Tobias Diedrich <ranma@gmx.at>
+ *                  Christian Garbs <mitch@cgarbs.de>
  * Licensed under GNU GPL.
  */
 
@@ -15,16 +16,60 @@
 #include "gbcpu.h"
 #include "gbs.h"
 
+/* global variables */
+char *myname;
+
+void usage(int exitcode)
+{
+        FILE *out = exitcode ? stderr : stdout;
+        fprintf(out,
+                _("Usage: %s [option] <gbs-file>\n"
+		  "\n"
+		  "Available options are:\n"
+		  "  -h  display this help and exit\n"
+		  "  -V  print version and exit\n"),
+                myname);
+        exit(exitcode);
+}
+
+void version(void)
+{
+	puts("gbsplay " GBS_VERSION);
+	exit(0);
+}
+
+void parseopts(int *argc, char ***argv)
+{
+	int res;
+	myname = *argv[0];
+	while ((res = getopt(*argc, *argv, "hV")) != -1) {
+		switch (res) {
+		default:
+			usage(1);
+			break;
+		case 'h':
+			usage(0);
+			break;
+		case 'V':
+			version();
+			break;
+		}
+	}
+	*argc -= optind;
+	*argv += optind;
+}
+
 int main(int argc, char **argv)
 {
 	struct gbs *gbs;
 
 	i18n_init();
 
-	if (argc != 2) {
-		printf(_("Usage: %s <gbs-file>\n"), argv[0]);
-		exit(1);
-	}
+        parseopts(&argc, &argv);
+	
+        if (argc < 1) {
+                usage(1);
+        }
 
 	if ((gbs = gbs_open(argv[1])) == NULL) exit(1);
 	gbs_printinfo(gbs, 1);
