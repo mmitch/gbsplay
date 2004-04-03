d37effbe599cc82b1cb7407e39a8acd22f878332
mitch <mitch>
2004-04-03 13:56:06 +0000

    massive code changes (still does not work)

--- plugout_nas.c
+++ plugout_nas.c
@@ -1,31 +1,64 @@
-/* $Id: plugout_nas.c,v 1.2 2004/04/02 14:25:34 mitch Exp $
+/* $Id: plugout_nas.c,v 1.3 2004/04/03 13:56:06 mitch Exp $
  *
  * gbsplay is a Gameboy sound player
  *
  * 2004 (C) by Christian Garbs <mitch@cgarbs.de>
- *             Tobias Diedrich <ranma@gmx.at>
  * Licensed under GNU GPL.
  *
  * NAS sound output plugin
  */
 
+
+
+/*****************************************************************************
+ * This code is heavily based on the XMMS NAS output plugin and the
+ * audiooss library, which are both GNU GPL'ed.  See copyrights below:
+ */
+
+/* NAS layer of libaudiooss. Large portions stolen from the XMMS NAS plugin,
+   which has the credits listed below. Adapted by Jon Trulson, further
+   modified by Erik Inge Bolsø. Largely rewritten afterwards by
+   Tobias Diedrich. Modifications during 2000-2002. */
+
+/*\  XMMS - Cross-platform multimedia player
+|*|  Copyright (C) 1998-1999  Peter Alm, Mikael Alm, Olle Hallnas,
+|*|                           Thomas Nilsson and 4Front Technologies
+|*|
+|*|  Network Audio System driver by Willem Monsuwe (willem@stack.nl)
+\*/
+
+/*****************************************************************************/
+
+
 #include "common.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <audio/audiolib.h>
 
 #include "plugout.h"
 
 
+#define DEBUG 1
+
+#define FRAG_SIZE 4096
+#define FRAG_COUNT 8
+#define STARTUP_THRESHOLD       FRAG_SIZE * 4
+#define MIN_BUFFER_SIZE FRAG_SIZE * FRAG_COUNT
+
+
 /* global variables */
 static AuServer      *nas_server;
 static AuFlowID       nas_flow;
 static AuDeviceID     nas_device;
-static AuUint32       nas_buffer_free = 0;
 static unsigned char  nas_format = AuFormatLinearSigned16LSB;
-
+static int            nas_client_buffer_size = 0;
+static int            nas_client_buffer_used = 0;
+static char          *nas_client_buffer = NULL;
+static int            nas_server_buffer_size = MIN_BUFFER_SIZE;
+static int            nas_server_buffer_used = 0;
 
 /* forward function declarations */
 static int     regparm nas_open(int endian, int rate);
@@ -43,7 +76,82 @@ struct output_plugin plugout_nas = {
 
 
 
-static char *nas_error(AuStatus status)
+/* begin of DEBUG code */
+
+#if DEBUG != 1
+#define DPRINTF(format, args...)
+#else
+#define DPRINTF(format, args...)        fprintf(stderr, format, ## args); \
+                                        fflush(stderr)
+
+static char *nas_event_types[] = {
+        "Undefined",
+        "Undefined",
+        "ElementNotify",
+        "GrabNotify",
+        "MonitorNotify",
+        "BucketNotify",
+        "DeviceNotify"
+};
+
+static char *nas_elementnotify_kinds[] = {
+        "LowWater",
+        "HighWater",
+        "State",
+        "Unknown"
+};
+
+static char *nas_states[] = {
+        "Stop",
+        "Start",
+        "Pause",
+        "Any"
+};
+
+static char *nas_reasons[] = {
+        "User",
+        "Underrun",
+        "Overrun",
+        "EOF",
+        "Watermark",
+        "Hardware",
+        "Any"
+};
+
+
+static char* regparm nas_reason(unsigned int reason)
+/* translate NAS reason */
+{
+        if (reason > 6) reason = 6;
+        return nas_reasons[reason];
+}
+
+static char* regparm nas_elementnotify_kind(unsigned int kind)
+/* translate NAS elementnotify kinf */
+{
+        if (kind > 2) kind = 3;
+        return nas_elementnotify_kinds[kind];
+}
+
+static char* regparm nas_event_type(unsigned int type)
+/* translate NAS event type */
+{
+        if (type > 6) type = 0;
+        return nas_event_types[type];
+}
+
+static char* regparm nas_state(unsigned int state)
+/* translate NAS state */
+{
+        if (state > 3) state = 3;
+        return nas_states[state];
+}
+
+#endif
+/* end of DEBUG code */
+
+
+static char regparm *nas_error(AuStatus status)
 /* convert AuStatus to error string */
 {
         static char s[100];
@@ -53,7 +161,31 @@ static char *nas_error(AuStatus status)
         return s;
 }
 
-static AuDeviceID nas_find_device(AuServer *aud)
+static void regparm nas_wait_for_event()
+/* wait for NAS event */
+{
+        AuEvent ev;
+
+        AuNextEvent(nas_server, AuTrue, &ev);
+        AuDispatchEvent(nas_server, &ev);
+}
+
+
+static void regparm nas_check_event_queue()
+/* check for audio events */
+{
+        AuEvent ev;
+        AuBool result;
+
+        do {
+                result = AuScanForTypedEvent(nas_server, AuEventsQueuedAfterFlush,
+					     AuTrue, AuEventTypeElementNotify, &ev);
+                if (result == AuTrue)
+                        AuDispatchEvent(nas_server, &ev);
+        } while (result == AuTrue);
+}
+
+static AuDeviceID regparm nas_find_device(AuServer *aud)
 /* look for a NAS device that supports 2 channels (we need stereo sound) */
 {
         int i;
@@ -67,45 +199,128 @@ static AuDeviceID nas_find_device(AuServer *aud)
         return AuNone;
 }
 
-static AuBool nas_event_handler(AuServer *srv, AuEvent *ev, AuEventHandlerRec *hnd)
+static void regparm nas_buffer_resize(int newlen)
+/* resize the client buffer */
+{
+        void *newbuf = malloc(nas_client_buffer_size = MAX(newlen, MIN_BUFFER_SIZE));
+        void *oldbuf = nas_client_buffer;
+
+        memcpy(newbuf, oldbuf, nas_client_buffer_used);
+        nas_client_buffer = newbuf;
+        if (oldbuf != NULL)
+                free(oldbuf);
+}
+
+static int regparm nas_readBuffer(int num)
+/* get buffer size */
+{
+        AuStatus as;
+
+        if (!nas_client_buffer)
+                nas_buffer_resize(MIN_BUFFER_SIZE);
+
+        DPRINTF("readBuffer(): num=%d client=%d/%d server=%d/%d\n",
+		num,
+		nas_client_buffer_used, nas_client_buffer_size,
+		nas_server_buffer_used, nas_server_buffer_size);
+
+        if (nas_client_buffer_used < num)
+                num = nas_client_buffer_used;
+
+        if (nas_client_buffer_used == 0) {
+                return 0;
+        }
+
+        AuWriteElement(nas_server, nas_flow, 0, num, nas_client_buffer, AuFalse, &as);
+        if (as == AuSuccess) {
+                nas_client_buffer_used -= num;
+                nas_server_buffer_used += num;
+/*                gettimeofday(&last_tv, 0); */
+                memmove(nas_client_buffer, nas_client_buffer + num, nas_client_buffer_used);
+/*                bytes_written += num; */
+        } else {
+                fprintf(stderr, _("%s:readBuffer: AuWriteElement %s\n"), plugout_nas.name, nas_error(as));
+                num = 0;
+        }
+        AuFlush(nas_server);
+
+        return num;
+}
+
+static void regparm nas_writeBuffer(const void *data, int len)
+/* write data to the server */
+{
+        if (len > nas_client_buffer_size)
+                nas_buffer_resize(len);
+
+        DPRINTF("writeBuffer(): len=%d client=%d/%d\n",
+		len, nas_client_buffer_used, nas_client_buffer_size);
+
+        memcpy(nas_client_buffer + nas_client_buffer_used, data, len);
+        nas_client_buffer_used += len;
+
+        if ((nas_server_buffer_used < nas_server_buffer_size) &&
+            ((nas_server_buffer_used != 0) ||
+             (nas_client_buffer_used > STARTUP_THRESHOLD))) {
+                AuSync(nas_server, AuFalse);
+                nas_check_event_queue();
+                nas_readBuffer(nas_server_buffer_size - nas_server_buffer_used);
+        }
+}
+
+
+static AuBool regparm nas_event_handler(AuServer* aud, AuEvent* ev, AuEventHandlerRec* hnd)
 /* handle NAS events */
 {
-	printf("EVENT!!!\n");
-	switch (ev->type)
-	{
-	case AuEventTypeElementNotify: {
-		AuElementNotifyEvent* event = (AuElementNotifyEvent *)ev;
-		switch (event->kind)
-		{
-		case AuElementNotifyKindLowWater:
-			printf("LOWWATER:%d\n", event->num_bytes);
-			if (nas_buffer_free >= 0) {
-				nas_buffer_free += event->num_bytes;
-			} else {
-				nas_buffer_free = event->num_bytes;
-			}
-			break;
-		case AuElementNotifyKindState:
-			printf("STATE:%d\n", event->num_bytes);
-			switch (event->cur_state)
-			{
-			case AuStatePause:
-				if (event->reason != AuReasonUser) {
-					if (nas_buffer_free >= 0) {
-						nas_buffer_free += event->num_bytes;
-					} else {
-						nas_buffer_free = event->num_bytes;
-					}
-				}
-				break;
-			}
-			break;
-		}
-		break;
-	}
-	}
-	
-	return AuTrue;
+        switch (ev->type) {
+        case AuEventTypeElementNotify: {
+                AuElementNotifyEvent* event = (AuElementNotifyEvent *)ev;
+
+                DPRINTF("event_handler(): kind %s state %s->%s reason %s numbytes %ld\n",
+                        nas_elementnotify_kind(event->kind),
+                        nas_state(event->prev_state),
+                        nas_state(event->cur_state),
+                        nas_reason(event->reason),
+                        event->num_bytes);
+
+                nas_server_buffer_used -= event->num_bytes;
+                if (nas_server_buffer_used < 0)
+                        nas_server_buffer_used = 0;
+
+                switch (event->kind) {
+                case AuElementNotifyKindLowWater:
+                        nas_readBuffer(event->num_bytes);
+                        break;
+                case AuElementNotifyKindState:
+                        if ((event->cur_state == AuStatePause) &&
+                            (event->reason == AuReasonUnderrun))
+                                /* buffer underrun -> refill buffer
+                                   we may have data in flight on its way
+                                   to the server, so do not write a full
+                                   buffer, only what the server asks for */
+/*                                if (!close) */ /* don't complain if we expect it */
+/*                                        fprintf(stderr,
+					  _("%s: buffer underrun\n"), plugout_nas.name); */
+                                nas_readBuffer(event->num_bytes);
+                        break;
+                }
+                break;
+        }
+        default:
+#if DEBUG != 1
+                fprintf(stderr,
+                        _("%s: event_handler: unhandled event type %d\n"),
+			plugout_nas.name,
+                        ev->type);
+                fflush(stderr);
+#else
+		DPRINTF ("%s: event_handler: unhandled event type %d (%s)\n",
+			plugout_nas.name, ev->type, nas_event_type(ev->type));
+
+#endif
+                break;
+        }
+        return AuTrue;
 }
 
 static int regparm nas_open(int endian, int rate)
@@ -151,10 +366,9 @@ static int regparm nas_open(int endian, int rate)
 	}
 
 	/* register event handler */
-/*	AuRegisterEventHandler(nas_server, AuEventHandlerIDMask, 0, nas_flow,
+	AuRegisterEventHandler(nas_server, AuEventHandlerIDMask | AuEventHandlerTypeMask, AuEventTypeElementNotify, nas_flow,
 			       nas_event_handler, (AuPointer) NULL);
-	nas_buffer_free = -1;
-*/
+
 	/* start sound flow */
         AuStartFlow(nas_server, nas_flow, &status);
         if (status != AuSuccess) {
@@ -168,20 +382,28 @@ static int regparm nas_open(int endian, int rate)
 static ssize_t regparm nas_write(const void *buf, size_t count)
 /* write audio data to NAS device */
 {
-	AuStatus status;
+        int writelen = 0;
+        int numwritten = 0;
 
-/*        while (count > nas_buffer_free) {
-		/* not all data fits in buffer * /
-                AuEvent ev;
-                AuNextEvent(nas_server, AuTrue, &ev);
-                AuDispatchEvent(nas_server, &ev);
-		printf(".");
+        DPRINTF("nas_write(): len=%d client=%d/%d server=%d/%d\n",
+		count, nas_client_buffer_used, nas_client_buffer_size,
+		nas_server_buffer_used, nas_server_buffer_size);
+
+        if (nas_client_buffer_size == 0)
+                nas_buffer_resize(MIN_BUFFER_SIZE);
+
+        while (numwritten < count) {
+                while (nas_client_buffer_size == nas_client_buffer_used)
+                        nas_wait_for_event();
+                if ((count - numwritten) > (nas_client_buffer_size - nas_client_buffer_used))
+                        writelen = nas_client_buffer_size - nas_client_buffer_used;
+                else writelen = (count - numwritten);
+                nas_writeBuffer(buf + numwritten, writelen);
+                numwritten += writelen;
         }
-*/
-        nas_buffer_free -= count;
-        AuWriteElement(nas_server, nas_flow, 0, count, (AuPointer*) buf, AuFalse, &status);
-	printf("%s\n", nas_error(status));
-	return count;
+
+        AuSync(nas_server, AuFalse);
+        return numwritten;
 }
 
 static void regparm nas_close()
