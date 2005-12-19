d1e49158d37e1d6a0438bf4f2f98f147aba241c1
ranmachan <ranmachan>
2005-12-19 20:48:37 +0000

    revert 'zelda fix'

--- gbhw.c
+++ gbhw.c
@@ -1,4 +1,4 @@
-/* $Id: gbhw.c,v 1.40 2005/12/19 20:48:10 ranmachan Exp $
+/* $Id: gbhw.c,v 1.41 2005/12/19 20:48:37 ranmachan Exp $
  *
  * gbsplay is a Gameboy sound player
  *
@@ -357,7 +357,6 @@ static regparm void io_put(uint32_t addr, uint8_t val)
 				unsigned long div = ioregs[0x13 + 5*chn];
 
 				div |= ((long)ioregs[0x14 + 5*chn] & 7) << 8;
-				div = (div + 1) & 2047;
 				gbhw_ch[chn].div_tc = 2048 - div;
 				gbhw_ch[chn].duty_tc = gbhw_ch[chn].div_tc*gbhw_ch[chn].duty_ctr/8;
 
