mmitch/gbsplay

commit 9b61a3842ec37bc380c20eaccef9012798e505ea

Support transparent gzip decompression on open

gbs.c
@@ -22,9 +22,22 @@
 #include "gbs.h"
 #include "crc32.h"
 
+#ifdef USE_ZLIB
+#include <zlib.h>
+#endif
+
+/* Max GB rom size is 4MiB (mapper with 256 banks) */
+#define GB_MAX_ROM_SIZE (256 * 0x4000)
+
+#define HDR_LEN_GBS	0x70
+#define HDR_LEN_GBR	0x20
+#define HDR_LEN_GB	0x20
+#define HDR_LEN_GZIP	10
+
 #define GBS_MAGIC		"GBS"
 #define GBS_EXTHDR_MAGIC	"GBSX"
 #define GBR_MAGIC		"GBRF"
+#define GZIP_MAGIC		"\037\213\010"
 
 const char *boot_rom_file = ".dmg_rom.bin";
 
@@ -327,12 +340,12 @@ regparm long gbs_write(struct gbs *gbs, char *name, long version)
 	return 1;
 }
 
-static regparm struct gbs *gb_open(char *name)
+static regparm struct gbs *gb_open(const char *name, char *buf, size_t size)
 {
-	long fd, i, name_len;
-	struct stat st;
+	int fd;
+	long i, name_len;
 	struct gbs *gbs = malloc(sizeof(struct gbs));
-	char *buf, *bootname;
+	char *bootname;
 	uint8_t bootrom[256];
 	char *na_str = _("gb / not available");
 
@@ -341,18 +354,6 @@ static regparm struct gbs *gb_open(char *name)
 	gbs->subsong_timeout = 2*60;
 	gbs->gap = 2;
 	gbs->fadeout = 3;
-	if ((fd = open(name, O_RDONLY)) == -1) {
-		fprintf(stderr, _("Could not open %s: %s\n"), name, strerror(errno));
-		gbs_free(gbs);
-		return NULL;
-	}
-	fstat(fd, &st);
-	gbs->buf = buf = malloc(st.st_size);
-	if (read(fd, buf, st.st_size) != st.st_size) {
-		fprintf(stderr, _("Could not read %s: %s\n"), name, strerror(errno));
-		gbs_free(gbs);
-		return NULL;
-	}
 	gbs->version = 0;
 	gbs->songs = 1;
 	gbs->defaultsong = 1;
@@ -388,11 +389,11 @@ static regparm struct gbs *gb_open(char *name)
 	gbs->author = na_str;
 	gbs->copyright = na_str;
 	gbs->code = buf;
-	gbs->filesize = st.st_size;
+	gbs->filesize = size;
 
 	gbs->subsong_info = malloc(gbs->songs * sizeof(struct gbs_subsong_info));
 	memset(gbs->subsong_info, 0, gbs->songs * sizeof(struct gbs_subsong_info));
-	gbs->codelen = st.st_size - 0x20;
+	gbs->codelen = size - 0x20;
 	gbs->crcnow = gbs_crc32(0, buf, gbs->filesize);
 	gbs->romsize = (gbs->codelen + 0x3fff) & ~0x3fff;
 
@@ -404,12 +405,10 @@ static regparm struct gbs *gb_open(char *name)
 	return gbs;
 }
 
-static regparm struct gbs *gbr_open(char *name)
+static regparm struct gbs *gbr_open(const char *name, char *buf, size_t size)
 {
-	long fd, i;
-	struct stat st;
+	long i;
 	struct gbs *gbs = malloc(sizeof(struct gbs));
-	char *buf;
 	char *na_str = _("gbr / not available");
 	uint16_t vsync_addr;
 	uint16_t timer_addr;
@@ -419,18 +418,6 @@ static regparm struct gbs *gbr_open(char *name)
 	gbs->subsong_timeout = 2*60;
 	gbs->gap = 2;
 	gbs->fadeout = 3;
-	if ((fd = open(name, O_RDONLY)) == -1) {
-		fprintf(stderr, _("Could not open %s: %s\n"), name, strerror(errno));
-		gbs_free(gbs);
-		return NULL;
-	}
-	fstat(fd, &st);
-	gbs->buf = buf = malloc(st.st_size);
-	if (read(fd, buf, st.st_size) != st.st_size) {
-		fprintf(stderr, _("Could not read %s: %s\n"), name, strerror(errno));
-		gbs_free(gbs);
-		return NULL;
-	}
 	if (strncmp(buf, GBR_MAGIC, 4) != 0) {
 		fprintf(stderr, _("Not a GBR-File: %s\n"), name);
 		gbs_free(gbs);
@@ -478,11 +465,11 @@ static regparm struct gbs *gbr_open(char *name)
 	gbs->author = na_str;
 	gbs->copyright = na_str;
 	gbs->code = &buf[0x20];
-	gbs->filesize = st.st_size;
+	gbs->filesize = size;
 
 	gbs->subsong_info = malloc(gbs->songs * sizeof(struct gbs_subsong_info));
 	memset(gbs->subsong_info, 0, gbs->songs * sizeof(struct gbs_subsong_info));
-	gbs->codelen = st.st_size - 0x20;
+	gbs->codelen = size - 0x20;
 	gbs->crcnow = gbs_crc32(0, buf, gbs->filesize);
 	gbs->romsize = (gbs->codelen + 0x3fff) & ~0x3fff;
 
@@ -503,19 +490,16 @@ static regparm struct gbs *gbr_open(char *name)
 		gbs->rom[0x51] = timer_addr & 0xff;
 		gbs->rom[0x52] = timer_addr >> 8;
 	}
-	close(fd);
 
 	return gbs;
 }
 
-regparm struct gbs *gbs_open(char *name)
+static regparm struct gbs *gbs_open_internal(const char *name, char *buf, size_t size)
 {
-	long fd, i;
-	struct stat st;
 	struct gbs *gbs = malloc(sizeof(struct gbs));
-	char *buf;
-	char *buf2 = NULL;
+	long i;
 	long have_ehdr = 0;
+	char *buf2;
 
 	memset(gbs, 0, sizeof(struct gbs));
 	gbs->silence_timeout = 2;
@@ -523,31 +507,6 @@ regparm struct gbs *gbs_open(char *name)
 	gbs->gap = 2;
 	gbs->fadeout = 3;
 	gbs->defaultbank = 1;
-	if ((fd = open(name, O_RDONLY)) == -1) {
-		fprintf(stderr, _("Could not open %s: %s\n"), name, strerror(errno));
-		gbs_free(gbs);
-		return NULL;
-	}
-	fstat(fd, &st);
-	gbs->buf = buf = malloc(st.st_size);
-	if (read(fd, buf, st.st_size) != st.st_size) {
-		fprintf(stderr, _("Could not read %s: %s\n"), name, strerror(errno));
-		gbs_free(gbs);
-		return NULL;
-	}
-	if (strncmp(buf, GBR_MAGIC, 4) == 0) {
-		gbs_free(gbs);
-		return gbr_open(name);
-	}
-	if (gbs_crc32(0, &buf[0x104], 48) == 0x46195417) {
-		gbs_free(gbs);
-		return gb_open(name);
-	}
-	if (strncmp(buf, GBS_MAGIC, 3) != 0) {
-		fprintf(stderr, _("Not a GBS-File: %s\n"), name);
-		gbs_free(gbs);
-		return NULL;
-	}
 	gbs->version = buf[0x03];
 	if (gbs->version != 1) {
 		fprintf(stderr, _("GBS Version %d unsupported.\n"), gbs->version);
@@ -583,7 +542,7 @@ regparm struct gbs *gbs_open(char *name)
 	gbs->author = gbs->v1strings+33;
 	gbs->copyright = gbs->v1strings+66;
 	gbs->code = &buf[0x70];
-	gbs->filesize = st.st_size;
+	gbs->filesize = size;
 
 	gbs->subsong_info = malloc(gbs->songs * sizeof(struct gbs_subsong_info));
 	memset(gbs->subsong_info, 0, gbs->songs * sizeof(struct gbs_subsong_info));
@@ -611,7 +570,7 @@ regparm struct gbs *gbs_open(char *name)
 		writeint(&buf2[0x10], 0, 4);
 	} else {
 		memcpy(gbs->v1strings+66, &buf[0x50], 32);
-		gbs->codelen = st.st_size - 0x70;
+		gbs->codelen = size - 0x70;
 	}
 	gbs->crcnow = gbs_crc32(0, buf, gbs->filesize);
 	if (have_ehdr) {
@@ -687,7 +646,98 @@ regparm struct gbs *gbs_open(char *name)
 	gbs->rom[0x58] = 0xd9; /* reti (Serial) */
 	gbs->rom[0x60] = 0xd9; /* reti (Joypad) */
 
-	close(fd);
+	return gbs;
+}
 
+#ifdef USE_ZLIB
+static regparm struct gbs *gzip_open(const char *name, char *buf, size_t size)
+{
+	struct gbs *gbs;
+	int ret;
+	char *out = malloc(GB_MAX_ROM_SIZE);
+	z_stream strm;
+
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	strm.next_in = (Bytef*)buf;
+	strm.avail_in = size;
+	strm.next_out = (Bytef*)out;
+	strm.avail_out = GB_MAX_ROM_SIZE;
+
+	/* inflate with gzip auto-detect */
+	ret = inflateInit2(&strm, 15|32);
+	if (ret != Z_OK) {
+		fprintf(stderr, _("Could not open %s: inflateInit2: %d\n"), name, ret);
+		return NULL;
+	}
+
+	ret = inflate(&strm, Z_FINISH);
+	if (ret != Z_STREAM_END) {
+		fprintf(stderr, _("Could not open %s: inflate: %d\n"), name, ret);
+		return NULL;
+	}
+	inflateEnd(&strm);
+	gbs = gbs_open_mem(name, out, GB_MAX_ROM_SIZE - strm.avail_out);
+	if (gbs == NULL) {
+		free(out);
+	}
+
+	return gbs;
+}
+#else
+static regparm struct gbs *gzip_open(char *name)
+{
+	fprintf(stderr, _("Could not open %s: %s\n"), name, _("Not compiled with zlib support"));
+	return NULL;
+}
+#endif
+
+regparm struct gbs *gbs_open_mem(const char *name, char *buf, size_t size)
+{
+	if (size > HDR_LEN_GZIP && strncmp(buf, GZIP_MAGIC, 3) == 0) {
+		return gzip_open(name, buf, size);
+	}
+	if (size > HDR_LEN_GBR && strncmp(buf, GBR_MAGIC, 4) == 0) {
+		return gbr_open(name, buf, size);
+	}
+	if (size > HDR_LEN_GBS && strncmp(buf, GBS_MAGIC, 3) == 0) {
+		return gbs_open_internal(name, buf, size);
+	}
+	if (size > HDR_LEN_GB && gbs_crc32(0, &buf[0x104], 48) == 0x46195417) {
+		return gb_open(name, buf, size);
+	}
+	fprintf(stderr, _("Not a GBS-File: %s\n"), name);
+	return NULL;
+}
+
+regparm struct gbs *gbs_open(const char *name)
+{
+	struct gbs *gbs = NULL;
+	long fd;
+	struct stat st;
+	char *buf;
+
+	if ((fd = open(name, O_RDONLY)) == -1) {
+		fprintf(stderr, _("Could not open %s: %s\n"), name, strerror(errno));
+		return NULL;
+	}
+	fstat(fd, &st);
+	buf = malloc(st.st_size);
+	if (read(fd, buf, st.st_size) != st.st_size) {
+		fprintf(stderr, _("Could not read %s: %s\n"), name, strerror(errno));
+		goto exit_free;
+	}
+	if (st.st_size > GB_MAX_ROM_SIZE) {
+		fprintf(stderr, _("Could not read %s: %s\n"), name, _("Bigger than allowed maximum (4MiB)"));
+		goto exit_free;
+	}
+
+	gbs = gbs_open_mem(name, buf, st.st_size);
+
+exit_free:
+	if (gbs == NULL || gbs->buf != buf)
+		free(buf);
+	close(fd);
 	return gbs;
 }

HISTORY
@@ -5,6 +5,11 @@ gbsplay HISTORY
 2020/xx/xx  -  unreleased
 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
+Enhancements:
+
+- gbsplay:
+  - transparent decompression of gzip-compressed files
+
 2020/06/26  -  0.0.94
 ~~~~~~~~~~~~~~~~~~~~~~~~~
 

configure
@@ -528,6 +528,7 @@ Optional Features:
   --disable-i18n         omit libintl support
   --disable-regparm      do not use register arguments on x86
   --disable-hardening    disable hardening flags
+  --disable-zlib         disable transparent gzip decompression
   --enable-debug         build with debug code
   --enable-sharedlibgbs  build libgbs as a shared library
   --enable-regparm       build with explicit regparm support
@@ -570,6 +571,7 @@ OPTS="${OPTS} use_pulse"
 OPTS="${OPTS} use_regparm"
 OPTS="${OPTS} use_sharedlibgbs"
 OPTS="${OPTS} use_stdout"
+OPTS="${OPTS} use_zlib"
 for OPT in $OPTS; do
     eval "${OPT}="
 done
@@ -669,6 +671,30 @@ fi
 ## check for various headers
 
 need_include inttypes.h
+
+if [ "$use_zlib" != no ]; then
+    remember_use zlib
+    check_include zlib.h
+    retval1=$?
+    retval2=1
+    if [ $retval1 -eq 0 ]; then
+        check_libs "z" <<EOF
+#include <zlib.h>
+int main(int argc, char** argv) {
+    z_stream strm = {0};
+    inflateInit(&strm);
+    return 0;
+}
+EOF
+        retval2=$?
+    fi
+    use_zlib=no
+    if [ "$retval1" -eq 0 ] && [ "$retval2" -eq 0 ]; then
+        use_zlib="$have_zlib_h"
+        append_nodupe LDFLAGS "-lz"
+    fi
+    recheck_use zlib
+fi
 
 if [ "$use_devdsp" != no ]; then
     remember_use devdsp
@@ -1036,6 +1062,7 @@ __EOF__
     plugout_x STDOUT
     use_x I18N
     use_x REGPARM
+    use_x ZLIB
     have_x ESTRPIPE
     echo "#endif"
 ) > config.h

gbs.h
@@ -60,7 +60,8 @@ struct gbs {
 	void *nextsubsong_cb_priv;
 };
 
-regparm /*@only@*/ /*@null@*/ struct gbs *gbs_open(char *name);
+regparm /*@only@*/ /*@null@*/ struct gbs *gbs_open(const char *name);
+regparm /*@only@*/ /*@null@*/ struct gbs *gbs_open_mem(const char *name, char *buf, size_t size);
 regparm long gbs_init(struct gbs *gbs, long subsong);
 regparm long gbs_step(struct gbs *gbs, long time_to_work);
 regparm void gbs_set_nextsubsong_cb(struct gbs *gbs, gbs_nextsubsong_cb cb, void *priv);

po/de.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: 0.0.4\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2019-11-24 21:04+0100\n"
+"POT-Creation-Date: 2020-06-27 21:28+0200\n"
 "PO-Revision-Date: 2015-07-18 22:22+0200\n"
 "Last-Translator: Christian Garbs <mitch@cgarbs.de>\n"
 "Language-Team: none\n"
@@ -98,6 +98,9 @@ msgstr ""
 msgid "Bank @0x4000:     %d\n"
 msgstr "Bank @0x4000:     %d\n"
 
+msgid "Bigger than allowed maximum (4MiB)"
+msgstr "Größer als erlaubtes Maximum (4MiB)"
+
 #, c-format
 msgid "CRC32:            0x%08lx\n"
 msgstr "CRC32:            0x%08lx\n"
@@ -115,6 +118,14 @@ msgstr "Copyright:"
 #, c-format
 msgid "Could not open %s: %s\n"
 msgstr "Konnte %s nicht öffnen: %s\n"
+
+#, c-format
+msgid "Could not open %s: inflate: %d\n"
+msgstr "Konnte %s nicht öffnen: inflate: %d\n"
+
+#, c-format
+msgid "Could not open %s: inflateInit2: %d\n"
+msgstr "Konnte %s nicht öffnen: inflateInit2: %d\n"
 
 #, c-format
 msgid "Could not open /dev/dsp: %s\n"
@@ -226,6 +237,9 @@ msgstr "Keine GBR-Datei: %s\n"
 #, c-format
 msgid "Not a GBS-File: %s\n"
 msgstr "Keine GBS-Datei: %s\n"
+
+msgid "Not compiled with zlib support"
+msgstr "Unterstützung für zlib nicht aktiviert"
 
 #, c-format
 msgid "Number of subsongs = %d is unreasonable.\n"

po/en.po
@@ -6,7 +6,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: 0.0.4\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2019-11-24 21:04+0100\n"
+"POT-Creation-Date: 2020-06-27 21:28+0200\n"
 "PO-Revision-Date: 2015-07-18 22:22+0200\n"
 "Last-Translator: Tobias Diedrich <ranma+gbsplay@tdiedrich.de>\n"
 "Language-Team: none\n"
@@ -97,6 +97,9 @@ msgstr ""
 msgid "Bank @0x4000:     %d\n"
 msgstr "Bank @0x4000:     %d\n"
 
+msgid "Bigger than allowed maximum (4MiB)"
+msgstr "Bigger than allowed maximum (4MiB)"
+
 #, c-format
 msgid "CRC32:            0x%08lx\n"
 msgstr "CRC32:            0x%08lx\n"
@@ -114,6 +117,14 @@ msgstr "Copyright:"
 #, c-format
 msgid "Could not open %s: %s\n"
 msgstr "Could not open %s: %s\n"
+
+#, c-format
+msgid "Could not open %s: inflate: %d\n"
+msgstr "Could not open %s: inflate: %d\n"
+
+#, c-format
+msgid "Could not open %s: inflateInit2: %d\n"
+msgstr "Could not open %s: inflateInit2: %d\n"
 
 #, c-format
 msgid "Could not open /dev/dsp: %s\n"
@@ -225,6 +236,9 @@ msgstr "Not a GBR-File: %s\n"
 #, c-format
 msgid "Not a GBS-File: %s\n"
 msgstr "Not a GBS-File: %s\n"
+
+msgid "Not compiled with zlib support"
+msgstr "Not compiled with zlib support"
 
 #, c-format
 msgid "Number of subsongs = %d is unreasonable.\n"

commit 34652b03b169da1fe51f8365a5e6f7a048655bc0

Fix use-after-free segfault in gb_open
Introduced in 9b61a3842ec37bc380c20eaccef9012798e505ea, which added
  if (gbs == NULL || gbs->buf != buf)
    free(buf);
in the open path.

gbs.c
@@ -365,6 +365,7 @@ static regparm struct gbs *gb_open(const char *name, char *buf, size_t size)
 	gbs->init  = 0x100;
 	gbs->play = gbs->init;
 	gbs->stack = 0xfffe;
+	gbs->buf = buf;
 
 	/* For accuracy testing purposes, support boot rom. */
 	name_len = strlen(getenv("HOME")) + strlen(boot_rom_file) + 2;