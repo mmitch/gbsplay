b1b519a382d4c5a653374ebb686805a94a4e34b9
Christian Garbs <mitch@cgarbs.de>
2020-11-05 19:45:08 +0100

    ensure expected build flags to be configured on the build servers

---
+++ check_buildflags.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+#
+# Ensure that configure enabled the plugouts as it was expected to.
+# This needs a bash with hashmap support but it is only used on the build machines
+#
+# 2020 (C) by Christian Garbs <mitch@cgarbs.de>
+# Licensed under GNU GPL v1 or, at your option, any later version.
+
+set -e
+
+die()
+{
+    echo "$@" >&2
+    exit 1
+}
+
+key_contains()
+{
+    local key="$1" searched="$2" 
+
+    local -a values
+    read -a values -r < <(grep "^$key := " config.mk)
+
+    local value
+    # :2 skips the first two array entries ($key and ":=")
+    for value in "${values[@]:2}"; do
+	if [ "$value" = "$searched" ]; then
+	    return 0
+	fi
+    done
+    return 1
+}
+
+expect_to_contain()
+{
+    local key="$1" expected="$2"
+    
+    if ! key_contains "$key" "$expected"; then
+	die "expected value <$expected> not found in key <$key>"
+    fi
+}
+
+expect_to_not_contain()
+{
+    local key="$1" unexpected="$2"
+
+    if key_contains "$key" "$unexpected"; then
+	die "unexpected value <$unexpected> found in key <$key>"
+    fi
+}
+
+# default configuration when all dependencies are available
+i18n=yes
+zlib=yes
+harden=yes
+sharedlib=no
+
+# parse build configuration
+for flag in "$@"; do
+    case "$flag" in
+	"")
+	;;
+	--disable-i18n)
+	    i18n=no
+	    ;;
+	--disable-zlib)
+	    zlib=no
+	    ;;
+	--disable-hardening)
+	    harden=no
+	    ;;
+	--enable-sharedlibgbs)
+	    sharedlib=yes
+	    ;;
+	*)
+	    die "unhandled flag <$flag>"
+	    ;;
+    esac
+done
+
+# test configuration
+expect_to_contain use_i18n "$i18n"
+
+if [ "$zlib" = yes ]; then
+    expect_to_contain EXTRA_LDFLAGS "-lz"
+else
+    expect_to_not_contain EXTRA_LDFLAGS "-lz"
+fi
+
+if [ "$harden" = yes ]; then
+    expect_to_contain EXTRA_CFLAGS "-fstack-protector-strong"
+    expect_to_contain EXTRA_LDFLAGS "-fstack-protector-strong"
+else
+    expect_to_not_contain EXTRA_CFLAGS "-fstack-protector"
+    expect_to_not_contain EXTRA_CFLAGS "-fstack-protector-strong"
+    expect_to_not_contain EXTRA_CFLAGS "-fstack-clash-protection"
+    expect_to_not_contain EXTRA_LDFLAGS "-fstack-protector"
+    expect_to_not_contain EXTRA_LDFLAGS "-fstack-protector-strong"
+    expect_to_not_contain EXTRA_LDFLAGS "-fstack-clash-protection"
+fi
+
+expect_to_contain use_sharedlibgbs "$sharedlib"
