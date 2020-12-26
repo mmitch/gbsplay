mmitch/gbsplay

commit 7e11d5a1ace6421e8c35991f71ffcf8fd289b3a4

refactor: don't expose struct gbs
this makes a cleaner interface where the internals can change without
affecting existing callers

player.c
@@ -63,10 +63,10 @@ static char *sound_name = PLUGOUT_DEFAULT;
 static char *filter_type = GBHW_CFG_FILTER_DMG;
 static char *sound_description;
 
-static struct gbhw_buffer buf = {
+static struct gbs_output_buffer buf = {
 	.data = NULL,
-	.pos  = 0,
 	.bytes = 8192,
+	.pos  = 0,
 };
 
 /* configuration directives */
@@ -86,7 +86,7 @@ const struct cfg_option options[] = {
 	{ NULL, NULL, NULL }
 };
 
-static void swap_endian(struct gbhw_buffer *buf)
+static void swap_endian(struct gbs_output_buffer *buf)
 {
 	long i;
 
@@ -101,7 +101,7 @@ static void iocallback(long cycles, uint32_t addr, uint8_t val, void *priv)
 	sound_io(cycles, addr, val);
 }
 
-static void callback(struct gbhw_buffer *buf, void *priv)
+static void callback(struct gbs_output_buffer *buf, void *priv)
 {
 	if ((is_le_machine() && endian == PLUGOUT_ENDIAN_BIG) ||
 	    (is_be_machine() && endian == PLUGOUT_ENDIAN_LITTLE)) {
@@ -132,30 +132,30 @@ long *setup_playlist(long songs)
 long get_next_subsong(struct gbs *gbs)
 /* returns the number of the subsong that is to be played next */
 {
+	const struct gbs_status *status = gbs_get_status(gbs);
 	long next = -1;
+
 	switch (playmode) {
 
 	case PLAYMODE_RANDOM:
-		next = rand_long(gbs->songs);
+		next = rand_long(status->songs);
 		break;
 
 	case PLAYMODE_SHUFFLE:
 		subsong_playlist_idx++;
-		if (subsong_playlist_idx == gbs->songs) {
+		if (subsong_playlist_idx == status->songs) {
 			free(subsong_playlist);
 			random_seed++;
-			subsong_playlist = setup_playlist(gbs->songs);
+			subsong_playlist = setup_playlist(status->songs);
 			subsong_playlist_idx = 0;
 		}
 		next = subsong_playlist[subsong_playlist_idx];
 		break;
 
 	case PLAYMODE_LINEAR:
-		next = gbs->subsong + 1;
+		next = status->subsong + 1;
 		break;
 	}
-
-	gbs->subsong %= gbs->songs;
 
 	return next;
 }
@@ -163,11 +163,13 @@ long get_next_subsong(struct gbs *gbs)
 int get_prev_subsong(struct gbs *gbs)
 /* returns the number of the subsong that has been played previously */
 {
+	const struct gbs_status *status = gbs_get_status(gbs);
 	int prev = -1;
+
 	switch (playmode) {
 
 	case PLAYMODE_RANDOM:
-		prev = rand_long(gbs->songs);
+		prev = rand_long(status->songs);
 		break;
 
 	case PLAYMODE_SHUFFLE:
@@ -175,65 +177,67 @@ int get_prev_subsong(struct gbs *gbs)
 		if (subsong_playlist_idx == -1) {
 			free(subsong_playlist);
 			random_seed--;
-			subsong_playlist = setup_playlist(gbs->songs);
-			subsong_playlist_idx = gbs->songs-1;
+			subsong_playlist = setup_playlist(status->songs);
+			subsong_playlist_idx = status->songs-1;
 		}
 		prev = subsong_playlist[subsong_playlist_idx];
 		break;
 
 	case PLAYMODE_LINEAR:
-		prev = gbs->subsong - 1;
+		prev = status->subsong - 1;
 		break;
-	}
-
-	while (gbs->subsong < 0) {
-		gbs->subsong += gbs->songs;
 	}
 
 	return prev;
 }
 
-static void setup_playmode(struct gbs *gbs)
+static long setup_playmode(struct gbs *gbs)
 /* initializes the chosen playmode (set start subsong etc.) */
 {
+	const struct gbs_status *status = gbs_get_status(gbs);
+	long subsong = status->subsong;
+
 	switch (playmode) {
 
 	case PLAYMODE_RANDOM:
-		if (gbs->subsong == -1) {
-			gbs->subsong = get_next_subsong(gbs);
+		if (subsong == -1) {
+			subsong = get_next_subsong(gbs);
 		}
 		break;
 
 	case PLAYMODE_SHUFFLE:
-		subsong_playlist = setup_playlist(gbs->songs);
+		subsong_playlist = setup_playlist(status->songs);
 		subsong_playlist_idx = 0;
-		if (gbs->subsong == -1) {
-			gbs->subsong = subsong_playlist[0];
+		if (subsong == -1) {
+			subsong = subsong_playlist[0];
 		} else {
 			/* randomize playlist until desired start song is first */
 			/* (rotation does not work because this must be reproducible */
 			/* by setting random_seed to the old value */
-			while (subsong_playlist[0] != gbs->subsong) {
+			while (subsong_playlist[0] != subsong) {
 				random_seed++;
-				subsong_playlist = setup_playlist(gbs->songs);
+				subsong_playlist = setup_playlist(status->songs);
 			}
 		}
 		break;
 
 	case PLAYMODE_LINEAR:
-		if (gbs->subsong == -1) {
-			gbs->subsong = gbs->defaultsong - 1;
+		if (subsong == -1) {
+			subsong = status->defaultsong - 1;
 		}
 		break;
 	}
+
+	return subsong;
 }
 
 long nextsubsong_cb(struct gbs *gbs, void *priv)
 {
+	const struct gbs_status *status = gbs_get_status(gbs);
 	long subsong = get_next_subsong(gbs);
 
-	if (gbs->subsong == subsong_stop ||
-	    subsong >= gbs->songs) {
+	if (status->subsong == subsong_stop ||
+	    subsong >= status->songs) {
 		if (loopmode) {
 			subsong = subsong_start;
 			setup_playmode(gbs);
@@ -248,10 +252,10 @@ long nextsubsong_cb(struct gbs *gbs, void *priv)
 	return true;
 }
 
-void update_displaytime(struct displaytime *time, struct gbs *gbs)
+void update_displaytime(struct displaytime *time, const struct gbs_status *status)
 {
-	long played = gbs->ticks / GBHW_CLOCK;
-	long total = gbs->subsong_info[gbs->subsong].len / 1024;
+	long played = status->ticks / GBHW_CLOCK;
+	long total = status->subsong_len / 1024;
 
 	time->played_min = played / 60;
 	time->played_sec = played % 60;
@@ -447,7 +451,8 @@ struct gbs *common_init(int argc, char **argv)
 {
 	char *usercfg;
 	struct gbs *gbs;
-	struct gbhw *gbhw;
+	uint8_t songs;
+	uint8_t initial_subsong;
 
 	i18n_init();
 
@@ -491,45 +496,38 @@ struct gbs *common_init(int argc, char **argv)
 		exit(1);
 	}
 
-	gbhw = &gbs->gbhw;
-
 	if (sound_io)
-		gbhw_setiocallback(gbhw, iocallback, NULL);
+		gbs_set_gbhw_io_callback(gbs, iocallback, NULL);
 	if (sound_write)
-		gbhw_setcallback(gbhw, callback, NULL);
-	gbhw_setrate(gbhw, rate);
-	if (!gbhw_setfilter(gbhw, filter_type)) {
+		gbs_set_sound_callback(gbs, callback, NULL);
+	gbs_configure_output(gbs, &buf, rate);
+	if (!gbs_set_gbhw_filter(gbs, filter_type)) {
 		fprintf(stderr, _("Invalid filter type \"%s\"\n"), filter_type);
 		exit(1);
 	}
 
 	/* sanitize commandline values */
+	songs = gbs_get_status(gbs)->songs;
 	if (subsong_start < -1) {
 		subsong_start = 0;
-	} else if (subsong_start >= gbs->songs) {
-		subsong_start = gbs->songs-1;
+	} else if (subsong_start >= songs) {
+		subsong_start = songs-1;
 	}
 	if (subsong_stop <  0) {
 		subsong_stop = -1;
-	} else if (subsong_stop >= gbs->songs) {
+	} else if (subsong_stop >= songs) {
 		subsong_stop = -1;
 	}
 
-	for (uint8_t ch = 0; ch < 4; ch++) {
-		gbhw->ch[ch].mute = mute_channel[ch];
-	}
+	// FIXME: proper configuration interface to gbs, this is just quickly slapped toghether
+	gbs_configure(gbs, subsong_start, subsong_timeout, silence_timeout, subsong_gap, fadeout);
+	gbs_configure_channels(gbs, mute_channel[0], mute_channel[1], mute_channel[2], mute_channel[3]);
 
-	gbs->subsong = subsong_start;
-	gbs->subsong_timeout = subsong_timeout;
-	gbs->silence_timeout = silence_timeout;
-	gbs->gap = subsong_gap;
-	gbs->fadeout = fadeout;
-	setup_playmode(gbs);
-	gbhw_setbuffer(gbhw, &buf);
 	gbs_set_nextsubsong_cb(gbs, nextsubsong_cb, NULL);
-	gbs_init(gbs, gbs->subsong);
+	initial_subsong = setup_playmode(gbs);
+	gbs_init(gbs, initial_subsong);
 	if (sound_skip)
-		sound_skip(gbs->subsong);
+		sound_skip(initial_subsong);
 	if (verbosity>0) {
 		gbs_printinfo(gbs, 0);
 	}

gbhw.c
@@ -82,7 +82,9 @@ struct gbhw* gbhw_create() {
 	return gbhw;
 }
 
-void gbhw_handle_init(struct gbhw *gbhw) {
+void gbhw_handle_init(struct gbhw *gbhw, struct gbs *gbs) {
+	gbhw->gbs = gbs;
+
 	gbhw->rombank = 1;
 	gbhw->apu_on = 1;
 	gbhw->io_written = 0;
@@ -709,7 +711,7 @@ static void gb_flush_buffer(struct gbhw *gbhw)
 	gbhw->soundbuf->l_cap = l_cap;
 	gbhw->soundbuf->r_cap = r_cap;
 
-	if (gbhw->callback != NULL) gbhw->callback(gbhw->soundbuf, gbhw->callbackpriv);
+	if (gbhw->callback != NULL) gbhw->callback(gbhw->gbs, gbhw->callbackpriv);
 
 	overlap = gbhw->impbuf->samples - gbhw->soundbuf->samples;
 	memmove(gbhw->impbuf->data, gbhw->impbuf->data+(2*gbhw->soundbuf->samples), 4*overlap);

gbhw.h
@@ -70,11 +70,14 @@ struct gbhw_channel {
 	long duty_ctr;
 };
 
-typedef void (*gbhw_callback_fn)(struct gbhw_buffer *buf, void *priv);
+struct gbs;
+
+typedef void (*gbhw_callback_fn)(struct gbs *gbs, void *priv);
 typedef void (*gbhw_iocallback_fn)(long cycles, uint32_t addr, uint8_t valu, void *priv);
 typedef void (*gbhw_stepcallback_fn)(const long cycles, const struct gbhw_channel[], void *priv);
 
 struct gbhw {
+	struct gbs *gbs; // FIXME: another circular backreference
 	uint8_t *rom;
 	long rombank;
 	long lastbank;
@@ -139,7 +142,7 @@ struct gbhw {
 };
 
 struct gbhw* gbhw_create();
-void gbhw_handle_init(struct gbhw *gbhw);
+void gbhw_handle_init(struct gbhw *gbhw, struct gbs *gbs);
 void gbhw_free(struct gbhw *gbhw);
 
 void gbhw_setcallback(struct gbhw *gbhw, gbhw_callback_fn fn, void *priv);

gbs.c
@@ -45,6 +45,87 @@
 
 const char *boot_rom_file = ".dmg_rom.bin";
 
+struct gbs_subsong_info {
+	uint32_t len;
+	char *title;
+};
+
+struct gbs {
+	char *buf;
+	uint8_t version;
+	uint8_t songs;
+	uint8_t defaultsong;
+	uint8_t defaultbank;
+	uint16_t load;
+	uint16_t init;
+	uint16_t play;
+	uint16_t stack;
+	uint8_t tma;
+	uint8_t tac;
+	char *title;
+	char *author;
+	char *copyright;
+	unsigned long codelen;
+	char *code;
+	char *exthdr;
+	size_t filesize;
+	uint32_t crc;
+	uint32_t crcnow;
+	struct gbs_subsong_info *subsong_info;
+	char *strings;
+	char v1strings[33*3];
+	uint8_t *rom;
+	unsigned long romsize;
+
+	long long ticks;
+	int16_t lmin, lmax, lvol, rmin, rmax, rvol;
+	long subsong_timeout, silence_timeout, fadeout, gap;
+	long long silence_start;
+	int subsong;
+
+	struct gbs_output_buffer *buffer;
+
+	gbs_sound_cb sound_cb;
+	void *sound_cb_priv;
+
+	gbs_nextsubsong_cb nextsubsong_cb;
+	void *nextsubsong_cb_priv;
+
+	struct gbs_status status;
+	struct gbhw_buffer gbhw_buf;
+	struct gbhw gbhw;
+};
+
+void gbs_configure(struct gbs *gbs, long subsong, long subsong_timeout, long silence_timeout, long subsong_gap, long fadeout)
+{
+	gbs->subsong = subsong;
+	gbs->subsong_timeout = subsong_timeout;
+	gbs->silence_timeout = silence_timeout;
+	gbs->gap = subsong_gap;
+	gbs->fadeout = fadeout;
+}
+
+void gbs_configure_channels(struct gbs *gbs, long mute_0, long mute_1, long mute_2, long mute_3) {
+	gbs->gbhw.ch[0].mute = mute_0;
+	gbs->gbhw.ch[1].mute = mute_1;
+	gbs->gbhw.ch[2].mute = mute_2;
+	gbs->gbhw.ch[3].mute = mute_3;
+}
+
+void gbs_configure_output(struct gbs *gbs, struct gbs_output_buffer *gbs_buf, long rate) {
+	struct gbhw_buffer *gbhw_buf = &gbs->gbhw_buf;
+
+	gbhw_setrate(&gbs->gbhw, rate);
+
+	gbs->buffer = gbs_buf;
+
+	gbhw_buf->data = gbs_buf->data;
+	gbhw_buf->bytes = gbs_buf->bytes;
+	gbhw_buf->pos = gbs_buf->pos;
+
+	gbhw_setbuffer(&gbs->gbhw, gbhw_buf);
+}
+
 long gbs_init(struct gbs *gbs, long subsong)
 {
 	struct gbhw *gbhw = &gbs->gbhw;
@@ -86,10 +167,41 @@ long gbs_init(struct gbs *gbs, long subsong)
 	return 1;
 }
 
+// FIXME: or just include a *ptr to the whole RAM in struct gbs_status?
+uint8_t gbs_io_peek(struct gbs *gbs, uint16_t addr) {
+	return gbhw_io_peek(&gbs->gbhw, addr);
+}
+
 void gbs_set_nextsubsong_cb(struct gbs *gbs, gbs_nextsubsong_cb cb, void *priv)
 {
 	gbs->nextsubsong_cb = cb;
 	gbs->nextsubsong_cb_priv = priv;
+}
+
+static void wrap_buffer_callback(struct gbs *gbs, void *priv)
+{
+	gbs->buffer->pos = gbs->gbhw_buf.pos;
+	gbs->sound_cb(gbs->buffer, priv);
+}
+
+void gbs_set_sound_callback(struct gbs *gbs, gbs_sound_cb fn, void *priv)
+{
+	gbs->sound_cb = fn;
+	gbhw_setcallback(&gbs->gbhw, wrap_buffer_callback, priv);
+}
+
+long gbs_set_gbhw_filter(struct gbs *gbs, const char *type) {
+	return gbhw_setfilter(&gbs->gbhw, type);
+}
+
+void gbs_set_gbhw_io_callback(struct gbs *gbs, gbhw_iocallback_fn fn, void *priv)
+{
+	gbhw_setiocallback(&gbs->gbhw, fn, priv);
+}
+
+void gbs_set_gbhw_step_callback(struct gbs *gbs, gbhw_stepcallback_fn fn, void *priv)
+{
+	gbhw_setstepcallback(&gbs->gbhw, fn, priv);
 }
 
 static long gbs_nextsubsong(struct gbs *gbs)
@@ -148,6 +260,11 @@ long gbs_step(struct gbs *gbs, long time_to_work)
 		return gbs_nextsubsong(gbs);
 
 	return true;
+}
+
+void gbs_pause(struct gbs *gbs, long new_pause)
+{
+	gbhw_pause(&gbs->gbhw, new_pause);
 }
 
 void gbs_printinfo(struct gbs *gbs, long verbose)
@@ -216,6 +333,56 @@ void gbs_printinfo(struct gbs *gbs, long verbose)
 			}
 		}
 	}
+}
+
+static long chvol(struct gbhw *gbhw, long ch)
+{
+	long v;
+
+	if (gbhw->ch[ch].mute ||
+	    gbhw->ch[ch].master == 0 ||
+	    (gbhw->ch[ch].leftgate == 0 &&
+	     gbhw->ch[ch].rightgate == 0)) return 0;
+
+	if (ch == 2)
+		v = (3-((gbhw->ch[2].env_volume+3)&3)) << 2;
+	else v = gbhw->ch[ch].env_volume;
+
+	return v;
+}
+
+const struct gbs_status* gbs_get_status(struct gbs *gbs) {
+
+	struct gbs_status *status = &gbs->status;
+	struct gbhw *gbhw = &gbs->gbhw;
+
+	// TODO: only update on songchange?
+	status->songtitle = gbs->subsong_info[gbs->subsong].title;
+	if (!status->songtitle) {
+		status->songtitle = _("Untitled");
+	}
+	status->subsong = gbs->subsong;
+	status->subsong_len = gbs->subsong_info[gbs->subsong].len;
+
+	// TODO: only update once
+	status->songs = gbs->songs;
+	status->defaultsong = gbs->defaultsong;
+
+	status->rvol = gbs->rvol;
+	status->lvol = gbs->lvol;
+	status->ticks = gbs->ticks;
+
+	for(long i = 0; i < 4; i++) {
+		status->ch[i].mute = gbhw->ch[i].mute;
+		status->ch[i].vol = chvol(gbhw, i);
+		status->ch[i].div_tc = gbhw->ch[i].div_tc;
+	}
+
+	return status;
+}
+
+long gbs_toggle_mute(struct gbs *gbs, long channel) {
+	return gbs->gbhw.ch[channel].mute ^= 1;
 }
 
 static void gbs_free(struct gbs *gbs)
@@ -809,7 +976,7 @@ static struct gbs *gbs_open_internal(const char *name, char *buf, size_t size)
 	char *buf2;
 
 	memset(gbs, 0, sizeof(struct gbs));
-	gbhw_handle_init(&gbs->gbhw);
+	gbhw_handle_init(&gbs->gbhw, gbs);
 	gbs->silence_timeout = 2;
 	gbs->subsong_timeout = 2*60;
 	gbs->gap = 2;

gbs.h
@@ -18,58 +18,52 @@
 #define GBS_LEN_DIV	(1 << GBS_LEN_SHIFT)
 
 struct gbs;
+struct gbs_output_buffer;
 
+typedef void (*gbs_sound_cb)(struct gbs_output_buffer *buf, void *priv);
 typedef long (*gbs_nextsubsong_cb)(struct gbs *gbs, void *priv);
 
-struct gbs_subsong_info {
-	uint32_t len;
-	char *title;
+struct gbs_output_buffer {
+	int16_t *data;
+	long bytes;
+	long pos;
 };
 
-struct gbs {
-	char *buf;
-	uint8_t version;
+struct gbs_status_ch {
+	long mute;
+	long vol;
+	long div_tc;
+};
+
+struct gbs_status {
+	char *songtitle;
+	int subsong;
+	uint32_t subsong_len;
 	uint8_t songs;
 	uint8_t defaultsong;
-	uint8_t defaultbank;
-	uint16_t load;
-	uint16_t init;
-	uint16_t play;
-	uint16_t stack;
-	uint8_t tma;
-	uint8_t tac;
-	char *title;
-	char *author;
-	char *copyright;
-	unsigned long codelen;
-	char *code;
-	char *exthdr;
-	size_t filesize;
-	uint32_t crc;
-	uint32_t crcnow;
-	struct gbs_subsong_info *subsong_info;
-	char *strings;
-	char v1strings[33*3];
-	uint8_t *rom;
-	unsigned long romsize;
-
+	long lvol;
+	long rvol;
 	long long ticks;
-	int16_t lmin, lmax, lvol, rmin, rmax, rvol;
-	long subsong_timeout, silence_timeout, fadeout, gap;
-	long long silence_start;
-	int subsong;
-	gbs_nextsubsong_cb nextsubsong_cb;
-	void *nextsubsong_cb_priv;
-
-	struct gbhw gbhw;
+	struct gbs_status_ch ch[4];
 };
 
 struct gbs *gbs_open(const char *name);
 struct gbs *gbs_open_mem(const char *name, char *buf, size_t size);
+void gbs_configure(struct gbs *gbs, long subsong, long subsong_timeout, long silence_timeout, long subsong_gap, long fadeout);
+void gbs_configure_channels(struct gbs *gbs, long mute_0, long mute_1, long mute_2, long mute_3);
+void gbs_configure_output(struct gbs *gbs, struct gbs_output_buffer *buf, long rate);
 long gbs_init(struct gbs *gbs, long subsong);
+uint8_t gbs_io_peek(struct gbs *gbs, uint16_t addr);
+const struct gbs_status* gbs_get_status(struct gbs *gbs);
 long gbs_step(struct gbs *gbs, long time_to_work);
 void gbs_set_nextsubsong_cb(struct gbs *gbs, gbs_nextsubsong_cb cb, void *priv);
+void gbs_set_sound_callback(struct gbs *gbs, gbs_sound_cb fn, void *priv);
+long gbs_set_gbhw_filter(struct gbs *gbs, const char *type); // FIXME: don't export gbhw
+void gbs_set_gbhw_io_callback(struct gbs *gbs, gbhw_iocallback_fn fn, void *priv); // FIXME: don't export gbhw
+void gbs_set_gbhw_step_callback(struct gbs *gbs, gbhw_stepcallback_fn fn, void *priv); // FIXME: don't export gbhw
+void gbs_pause(struct gbs *gbs, long new_pause);
 void gbs_printinfo(struct gbs *gbs, long verbose);
+long gbs_toggle_mute(struct gbs *gbs, long channel);
 void gbs_close(struct gbs *gbs);
 long gbs_write(struct gbs *gbs, char *name, long version);
 

gbsplay.c
@@ -103,22 +103,22 @@ static void stepcallback(long cycles, const struct gbhw_channel chan[], void *pr
 
 static void handleuserinput(struct gbs *gbs)
 {
-	struct gbhw *gbhw = &gbs->gbhw;
+	long subsong;
 	char c;
 
 	if (get_input(&c)) {
 		switch (c) {
 		case 'p':
-			gbs->subsong = get_prev_subsong(gbs);
-			gbs_init(gbs, gbs->subsong);
+			subsong = get_prev_subsong(gbs);
+			gbs_init(gbs, subsong);
 			if (sound_skip)
-				sound_skip(gbs->subsong);
+				sound_skip(subsong);
 			break;
 		case 'n':
-			gbs->subsong = get_next_subsong(gbs);
-			gbs_init(gbs, gbs->subsong);
+			subsong = get_next_subsong(gbs);
+			gbs_init(gbs, subsong);
 			if (sound_skip)
-				sound_skip(gbs->subsong);
+				sound_skip(subsong);
 			break;
 		case 'q':
 		case 27:
@@ -126,50 +126,31 @@ static void handleuserinput(struct gbs *gbs)
 			break;
 		case ' ':
 			pause_mode = !pause_mode;
-			gbhw_pause(gbhw, pause_mode);
+			gbs_pause(gbs, pause_mode);
 			if (sound_pause) sound_pause(pause_mode);
 			break;
 		case '1':
 		case '2':
 		case '3':
 		case '4':
-			gbhw->ch[c-'1'].mute ^= 1;
+			gbs_toggle_mute(gbs, c-'1');
 			break;
 		}
 	}
 }
 
 // TODO: only pass struct gbhw_channnel instead of struct gbhw?
-static char *notestring(struct gbhw *gbhw, long ch)
+static char *notestring(const struct gbs_status *status, long ch)
 {
 	long n;
 
-	if (gbhw->ch[ch].mute) return "-M-";
+	if (status->ch[ch].mute) return "-M-";
 
-	if (gbhw->ch[ch].env_volume == 0 ||
-	    gbhw->ch[ch].master == 0 ||
-	    (gbhw->ch[ch].leftgate == 0 &&
-	     gbhw->ch[ch].rightgate == 0)) return "---";
+	if (status->ch[ch].vol == 0) return "---";
 
-	n = getnote(gbhw->ch[ch].div_tc);
+	n = getnote(status->ch[ch].div_tc);
 	if (ch != 3) return &notelookup[4*n];
 	else return "nse";
-}
-
-static long chvol(struct gbhw *gbhw, long ch)
-{
-	long v;
-
-	if (gbhw->ch[ch].mute ||
-	    gbhw->ch[ch].master == 0 ||
-	    (gbhw->ch[ch].leftgate == 0 &&
-	     gbhw->ch[ch].rightgate == 0)) return 0;
-
-	if (ch == 2)
-		v = (3-((gbhw->ch[2].env_volume+3)&3)) << 2;
-	else v = gbhw->ch[ch].env_volume;
-
-	return v;
 }
 
 static char *volstring(long v)
@@ -180,57 +161,54 @@ static char *volstring(long v)
 	return &vollookup[5*v];
 }
 
-static void printregs(struct gbhw *gbhw)
+static void printregs(struct gbs *gbs)
 {
 	long i;
 	for (i=0; i<5*4; i++) {
 		if (i % 5 == 0)
 			printf("CH%ld:", i/5 + 1);
-		printf(" %02x", gbhw_io_peek(gbhw, 0xff10+i));
+		printf(" %02x", gbs_io_peek(gbs, 0xff10+i));
 		if (i % 5 == 4)
 			printf("\n");
 	}
 	printf("MISC:");
 	for (i+=0x10; i<0x27; i++) {
-		printf(" %02x", gbhw_io_peek(gbhw, 0xff00+i));
+		printf(" %02x", gbs_io_peek(gbs, 0xff00+i));
 	}
 	printf("\nWAVE: ");
 	for (i=0; i<16; i++) {
-		printf("%02x", gbhw_io_peek(gbhw, 0xff30+i));
+		printf("%02x", gbs_io_peek(gbs, 0xff30+i));
 	}
 	printf("\n\033[A\033[A\033[A\033[A\033[A\033[A");
 }
 
 static void printstatus(struct gbs *gbs)
 {
-	struct gbhw *gbhw = &gbs->gbhw;
+	const struct gbs_status *status;
 	struct displaytime time;
-	char *songtitle;
 
-	update_displaytime(&time, gbs);
+	status = gbs_get_status(gbs);
 
-	songtitle = gbs->subsong_info[gbs->subsong].title;
-	if (!songtitle) {
-		songtitle=_("Untitled");
-	}
+	update_displaytime(&time, status);
+
 	printf("\r\033[A\033[A"
 	       "Song %3d/%3d (%s)\033[K\n"
 	       "%02ld:%02ld/%02ld:%02ld",
-	       gbs->subsong+1, gbs->songs, songtitle,
+	       status->subsong+1, status->songs, status->songtitle,
 	       time.played_min, time.played_sec, time.total_min, time.total_sec);
 	if (verbosity>2) {
 		printf("  %s %s  %s %s  %s %s  %s %s  [%s|%s]\n",
-		       notestring(gbhw, 0), volstring(chvol(gbhw, 0)),
-		       notestring(gbhw, 1), volstring(chvol(gbhw, 1)),
-		       notestring(gbhw, 2), volstring(chvol(gbhw, 2)),
-		       notestring(gbhw, 3), volstring(chvol(gbhw, 3)),
-		       reverse_vol(volstring(gbs->lvol/1024)),
-		       volstring(gbs->rvol/1024));
+		       notestring(status, 0), volstring(status->ch[0].vol),
+		       notestring(status, 1), volstring(status->ch[1].vol),
+		       notestring(status, 2), volstring(status->ch[2].vol),
+		       notestring(status, 3), volstring(status->ch[3].vol),
+		       reverse_vol(volstring(status->lvol/1024)),
+		       volstring(status->rvol/1024));
 	} else {
 		puts("");
 	}
 	if (verbosity>3) {
-		printregs(gbhw);
+		printregs(gbs);
 	}
 	fflush(stdout);
 }
@@ -259,7 +237,7 @@ int main(int argc, char **argv)
 
 	/* init additional callbacks */
 	if (sound_step)
-		gbhw_setstepcallback(&gbs->gbhw, stepcallback, NULL);
+		gbs_set_gbhw_step_callback(gbs, stepcallback, NULL);
 
 	/* precalculate lookup tables */
 	precalc_notes();

player.h
@@ -54,7 +54,7 @@ struct displaytime {
 long get_next_subsong(struct gbs *gbs);
 int get_prev_subsong(struct gbs *gbs);
 long nextsubsong_cb(struct gbs *gbs, void *priv);
-void update_displaytime(struct displaytime *time, struct gbs *gbs);
+void update_displaytime(struct displaytime *time, const struct gbs_status *status);
 struct gbs *common_init(int argc, char **argv);
 void common_cleanup(struct gbs *gbs);
 

xgbsplay.c
@@ -29,14 +29,15 @@ static GC gc;
 
 static void updatetitle(struct gbs *gbs)
 {
+	const struct gbs_status *status = gbs_get_status(gbs);
 	struct displaytime time;
 
-	update_displaytime(&time, gbs);
+	update_displaytime(&time, status);
 
 	snprintf(statustext, STATUSTEXT_LENGTH, /* or use sizeof(statustext) ?? */
 		 "xgbsplay %s %d/%d "
 		 "%02ld:%02ld/%02ld:%02ld",
-		 filename, gbs->subsong+1, gbs->songs,
+		 filename, status->subsong+1, status->songs,
 		 time.played_min, time.played_sec, time.total_min, time.total_sec);
 
 	if (strncmp(statustext, oldstatustext, STATUSTEXT_LENGTH) != 0) {  /* or use sizeof(statustext) ?? or strcmp() ?? */
@@ -117,21 +118,22 @@ static void drawbuttons()
 
 static int handlebutton(XButtonEvent *xev, struct gbs *gbs)
 {
+	uint8_t subsong;
 	XWindowAttributes attrs;
 
 	XGetWindowAttributes(display, window, &attrs);
 
 	switch (xev->x * 4 / attrs.width) {
 	case 0: /* prev */
-		gbs->subsong = get_prev_subsong(gbs);
-		gbs_init(gbs, gbs->subsong);
+		subsong = get_prev_subsong(gbs);
+		gbs_init(gbs, subsong);
 		if (sound_skip)
-			sound_skip(gbs->subsong);
+			sound_skip(subsong);
 		break;
 
 	case 1: /* pause */
 		pause_mode = !pause_mode;
-		gbhw_pause(&gbs->gbhw, pause_mode);
+		gbs_pause(gbs, pause_mode);
 		if (sound_pause) sound_pause(pause_mode);
 		break;
 
@@ -139,10 +141,10 @@ static int handlebutton(XButtonEvent *xev, struct gbs *gbs)
 		return 1;
 
 	case 3: /* next */
-		gbs->subsong = get_next_subsong(gbs);
-		gbs_init(gbs, gbs->subsong);
+		subsong = get_next_subsong(gbs);
+		gbs_init(gbs, subsong);
 		if (sound_skip)
-			sound_skip(gbs->subsong);
+			sound_skip(subsong);
 		break;
 	}
 

commit 0ad4c6c604dfda9b6da3900d18041396d4f9f6bd

fix overflow on subsong change
Playing the next subsong in linear mode when already on the last
subsong tried to play an out-of-bounds subsong instead of wrapping
around to the first song.

bug was introduced with 7e11d5a1ace6421e8c35991f71ffcf8fd289b3a4

note: overflowclamping can't be included in get_next_subsong()
because nextsubsong_cb() calls that as well and expects the overflow
to happen to detect end of play

player.c
@@ -200,12 +200,19 @@ static void play_subsong(struct gbs *gbs, long subsong) {
 
 void play_next_subsong(struct gbs *gbs)
 {
-	play_subsong(gbs, get_next_subsong(gbs));
+	const struct gbs_status *status = gbs_get_status(gbs);
+	long next = get_next_subsong(gbs);
+	next %= status->songs;
+	play_subsong(gbs, next);
 }
 
 void play_prev_subsong(struct gbs *gbs)
 {
-	play_subsong(gbs, get_prev_subsong(gbs));
+	const struct gbs_status *status = gbs_get_status(gbs);
+	long prev = get_prev_subsong(gbs);
+	while (prev < 0)
+		prev += status->songs;
+	play_subsong(gbs, prev);
 }
 
 static long setup_playmode(struct gbs *gbs)