mmitch/gbsplay

commit c648a9f0d3b61c7f7967319c25c7eda27b39f5b9

fix sizeof() for arrays in structs

gbhw.c
@@ -227,7 +227,7 @@ static void apu_reset(struct gbhw *gbhw)
 	for (i = 0; i < 4; i++) {
 		mute_tmp[i] = gbhw->ch[i].mute;
 	}
-	memset(gbhw->ch, 0, sizeof(gbhw->ch));
+	memset(gbhw->ch, 0, sizeof(struct gbhw_channel) * 4);
 	for (i = 0xff10; i < 0xff26; i++) {
 		gbhw->ioregs[i & 0x7f] = 0;
 	}
@@ -955,10 +955,10 @@ void gbhw_init(struct gbhw *gbhw, uint8_t *rombuf, uint32_t size)
 	gbhw->lminval = gbhw->rminval = INT_MAX;
 	gbhw->lmaxval = gbhw->rmaxval = INT_MIN;
 	apu_reset(gbhw);
-	memset(gbhw->extram, 0, sizeof(gbhw->extram));
-	memset(gbhw->intram, 0, sizeof(gbhw->intram));
-	memset(gbhw->hiram, 0, sizeof(gbhw->hiram));
-	memset(gbhw->ioregs, 0, sizeof(gbhw->ioregs));
+	memset(gbhw->extram, 0, sizeof(uint8_t) * GBHW_EXTRAM_SIZE);
+	memset(gbhw->intram, 0, sizeof(uint8_t) * GBHW_INTRAM_SIZE);
+	memset(gbhw->hiram, 0, sizeof(uint8_t) * GBHW_HIRAM_SIZE);
+	memset(gbhw->ioregs, 0, sizeof(uint8_t) * GBHW_IOREGS_SIZE);
 	for (i=0x10; i<0x40; i++) {
 		io_put(gbhw, 0xff00 + i, ioregs_initdata[i]);
 	}
@@ -980,7 +980,7 @@ void gbhw_init(struct gbhw *gbhw, uint8_t *rombuf, uint32_t size)
 
 void gbhw_enable_bootrom(struct gbhw *gbhw, const uint8_t *rombuf)
 {
-	memcpy(gbhw->boot_rom, rombuf, sizeof(gbhw->boot_rom));
+	memcpy(gbhw->boot_rom, rombuf, sizeof(uint8_t) * GBHW_BOOT_ROM_SIZE);
 	gbhw->rom_lockout = 0;
 }
 

gbhw.h
@@ -25,7 +25,12 @@
 #define GBHW_FILTER_CONST_DMG 0.999958
 #define GBHW_FILTER_CONST_CGB 0.998943
 
+#define GBHW_HIRAM_SIZE 0x80
+#define GBHW_INTRAM_SIZE 0x200
+#define GBHW_EXTRAM_SIZE 0x200
 #define GBHW_IOREGS_SIZE 0x80
+
+#define GBHW_BOOT_ROM_SIZE 256
 
 struct gbhw_buffer {
 	int16_t *data;
@@ -125,12 +130,12 @@ struct gbhw {
 
 	struct gbhw_channel ch[4];
 
-	uint8_t hiram[0x80];
-	uint8_t intram[0x2000];
-	uint8_t extram[0x2000];
+	uint8_t hiram[GBHW_HIRAM_SIZE];
+	uint8_t intram[GBHW_INTRAM_SIZE];
+	uint8_t extram[GBHW_EXTRAM_SIZE];
 	uint8_t ioregs[GBHW_IOREGS_SIZE];
 
-	uint8_t boot_rom[256];
+	uint8_t boot_rom[GBHW_BOOT_ROM_SIZE];
 };
 
 struct gbhw* gbhw_create();

gbcpu.c
@@ -1608,7 +1608,7 @@ void gbcpu_addmem(struct gbcpu *gbcpu, uint32_t start, uint32_t end, gbcpu_put_f
 
 void gbcpu_init(struct gbcpu *gbcpu)
 {
-	memset(&gbcpu->regs, 0, sizeof(gbcpu->regs));
+	memset(&gbcpu->regs, 0, sizeof(gbcpu_regs_u));
 	gbcpu->halted = 0;
 	gbcpu->stopped = 0;
 	gbcpu->ie = 0;

commit b30273978da8259d051d83c7f5b22bc534f6cd30

Fix typo in memory size constants.
Bug introduced in c648a9f0d3b61c7f7967319c25c7eda27b39f5b9.
This was causing out-of-bound accesses (caught using valgrind).
Additionally, also derive the mask from the size for consistency.

gbhw.c
@@ -126,11 +126,11 @@ static uint32_t rombank_get(struct gbhw *gbhw, uint32_t addr)
 static uint32_t io_get(struct gbhw *gbhw, uint32_t addr)
 {
 	if (addr >= 0xff80 && addr <= 0xfffe) {
-		return gbhw->hiram[addr & 0x7f];
+		return gbhw->hiram[addr & GBHW_HIRAM_MASK];
 	}
 	if (addr >= 0xff10 &&
 	           addr <= 0xff3f) {
-		uint8_t val = gbhw->ioregs[addr & 0x7f];
+		uint8_t val = gbhw->ioregs[addr & GBHW_IOREGS_MASK];
 		if (addr == 0xff26) {
 			long i;
 			val &= 0xf0;
@@ -140,7 +140,7 @@ static uint32_t io_get(struct gbhw *gbhw, uint32_t addr)
 				}
 			}
 		}
-		val |= ioregs_ormask[addr & 0x7f];
+		val |= ioregs_ormask[addr & GBHW_IOREGS_MASK];
 		DPRINTF("io_get(%04x): %02x\n", addr, val);
 		return val;
 	}
@@ -151,7 +151,7 @@ static uint32_t io_get(struct gbhw *gbhw, uint32_t addr)
 	case 0xff06:  // TMA
 	case 0xff07:  // TAC
 	case 0xff0f:  // IF
-		return gbhw->ioregs[addr & 0x7f];
+		return gbhw->ioregs[addr & GBHW_IOREGS_MASK];
 	case 0xff41: /* LCDC Status */
 		if (gbhw->vblankctr > vblanktc - vblankclocks) {
 			return 0x01;  /* vblank */
@@ -184,13 +184,13 @@ static uint32_t io_get(struct gbhw *gbhw, uint32_t addr)
 static uint32_t intram_get(struct gbhw *gbhw, uint32_t addr)
 {
 //	DPRINTF("intram_get(%04x)\n", addr);
-	return gbhw->intram[addr & 0x1fff];
+	return gbhw->intram[addr & GBHW_INTRAM_MASK];
 }
 
 static uint32_t extram_get(struct gbhw *gbhw, uint32_t addr)
 {
 //	DPRINTF("extram_get(%04x)\n", addr);
-	return gbhw->extram[addr & 0x1fff];
+	return gbhw->extram[addr & GBHW_EXTRAM_MASK];
 }
 
 static void rom_put(struct gbhw *gbhw, uint32_t addr, uint8_t val)
@@ -217,7 +217,7 @@ static void apu_reset(struct gbhw *gbhw)
 	}
 	memset(gbhw->ch, 0, sizeof(struct gbhw_channel) * 4);
 	for (i = 0xff10; i < 0xff26; i++) {
-		gbhw->ioregs[i & 0x7f] = 0;
+		gbhw->ioregs[i & GBHW_IOREGS_MASK] = 0;
 	}
 	for (i = 0; i < 4; i++) {
 		gbhw->ch[i].len = 0;
@@ -304,7 +304,7 @@ static void io_put(struct gbhw *gbhw, uint32_t addr, uint8_t val)
 	long chn = (addr - 0xff10)/5;
 
 	if (addr >= 0xff80 && addr <= 0xfffe) {
-		gbhw->hiram[addr & 0x7f] = val;
+		gbhw->hiram[addr & GBHW_HIRAM_MASK] = val;
 		return;
 	}
 
@@ -316,7 +316,7 @@ static void io_put(struct gbhw *gbhw, uint32_t addr, uint8_t val)
 	if (gbhw->apu_on == 0 && addr >= 0xff10 && addr < 0xff26) {
 		return;
 	}
-	gbhw->ioregs[addr & 0x7f] = val;
+	gbhw->ioregs[addr & GBHW_IOREGS_MASK] = val;
 	DPRINTF(" ([0x%04x]=%02x) ", addr, val);
 	switch (addr) {
 		case 0xff02:
@@ -556,12 +556,12 @@ static void io_put(struct gbhw *gbhw, uint32_t addr, uint8_t val)
 
 static void intram_put(struct gbhw *gbhw, uint32_t addr, uint8_t val)
 {
-	gbhw->intram[addr & 0x1fff] = val;
+	gbhw->intram[addr & GBHW_INTRAM_MASK] = val;
 }
 
 static void extram_put(struct gbhw *gbhw, uint32_t addr, uint8_t val)
 {
-	gbhw->extram[addr & 0x1fff] = val;
+	gbhw->extram[addr & GBHW_EXTRAM_MASK] = val;
 }
 
 static void sequencer_step(struct gbhw *gbhw)
@@ -983,7 +983,7 @@ void gbhw_io_put(struct gbhw *gbhw, uint16_t addr, uint8_t val) {
 uint8_t gbhw_io_peek(struct gbhw *gbhw, uint16_t addr)
 {
 	if (addr >= 0xff10 && addr <= 0xff3f) {
-		return gbhw->ioregs[addr & 0x7f];
+		return gbhw->ioregs[addr & GBHW_IOREGS_MASK];
 	}
 	return 0xff;
 }

gbhw.h
@@ -25,10 +25,14 @@
 #define GBHW_FILTER_CONST_DMG 0.999958
 #define GBHW_FILTER_CONST_CGB 0.998943
 
-#define GBHW_HIRAM_SIZE 0x80
-#define GBHW_INTRAM_SIZE 0x200
-#define GBHW_EXTRAM_SIZE 0x200
+#define GBHW_INTRAM_SIZE 0x2000
+#define GBHW_INTRAM_MASK (GBHW_INTRAM_SIZE - 1)
+#define GBHW_EXTRAM_SIZE 0x2000
+#define GBHW_EXTRAM_MASK (GBHW_EXTRAM_SIZE - 1)
 #define GBHW_IOREGS_SIZE 0x80
+#define GBHW_IOREGS_MASK (GBHW_IOREGS_SIZE - 1)
+#define GBHW_HIRAM_SIZE  0x80
+#define GBHW_HIRAM_MASK  (GBHW_HIRAM_SIZE - 1)
 
 #define GBHW_BOOT_ROM_SIZE 256
 