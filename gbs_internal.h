3f4cb3ff6e1d14218dade73cc1d659c2e42d4ed4
Christian Garbs <mitch@cgarbs.de>
2021-06-21 22:18:20 +0200

    fix copyright year
    The initial addition of the file to the repository was in 2021.
    Setting the copyright to 2020 was a case of checkuary.

--- gbs_internal.h
+++ gbs_internal.h
@@ -1,7 +1,7 @@
 /*
  * gbsplay is a Gameboy sound player
  *
- * 2020-2021 (C) by Tobias Diedrich <ranma+gbsplay@tdiedrich.de>
+ * 2021 (C) by Tobias Diedrich <ranma+gbsplay@tdiedrich.de>
  *
  * Licensed under GNU GPL v1 or, at your option, any later version.
  */
