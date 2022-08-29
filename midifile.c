a2838935fa65e7bbf0b3a3826ade5e010402ba7e
Christian Garbs <mitch@cgarbs.de>
2022-08-29 21:08:54 +0200

    refactor midifile.c

--- midifile.c
+++ midifile.c
@@ -38,64 +38,68 @@ void midi_update_mute(const struct gbs_channel_status status[]) {
 		mute[chan] = status[chan].mute;
 }
 
-static int midi_write_u32(uint32_t x)
-{
-	uint8_t s[4];
-
-	s[0] = x >> 24;
-	s[1] = x >> 16;
-	s[2] = x >> 8;
-	s[3] = x;
-
-	if (fwrite(s, 1, 4, file) != 4)
+static int midi_write_data(const void *data, unsigned int length) {
+	if (fwrite(data, 1, length, file) != length)
 		return 1;
+
+	track_length += length;
 	return 0;
 }
 
-static int midi_write_u16(uint16_t x)
-{
-	uint8_t s[2];
-
-	s[0] = x >> 8;
-	s[1] = x;
-
-	if (fwrite(s, 1, 2, file) != 2)
-		return 1;
-	return 0;
+static int midi_write_string(const char *string) {
+	return midi_write_data(string, strlen(string));;
 }
 
-static int midi_write_varlen(unsigned long x)
+static int midi_write_u32(uint32_t value)
+{
+	uint8_t data[4];
+
+	data[0] = value >> 24;
+	data[1] = value >> 16;
+	data[2] = value >> 8;
+	data[3] = value;
+
+	return midi_write_data(data, 4);
+}
+
+static int midi_write_u16(uint16_t value)
+{
+	uint8_t data[2];
+
+	data[0] = value >> 8;
+	data[1] = value;
+
+	return midi_write_data(data, 2);
+}
+
+static int midi_write_varlen(unsigned long value)
 {
 	uint8_t data[4];
 	unsigned int i;
 
 	for (i = 0; i < 4; ++i) {
-		data[3 - i] = (x & 0x7f) | 0x80;
-		x >>= 7;
+		data[3 - i] = (value & 0x7f) | 0x80;
+		value >>= 7;
 
-		if (!x) {
+		if (!value) {
 			++i;
 			break;
 		}
 	}
 
 	data[3] &= ~0x80;
-	if (fwrite(data + 4 - i, 1, i, file) != i)
-		return 1;
 
-	track_length += i;
-	return 0;
+	return midi_write_data(data + 4 - i, i);
 }
 
-static int midi_write_event(cycles_t cycles, const uint8_t *s, unsigned int n)
+static int midi_write_event(cycles_t cycles, const uint8_t *data, unsigned int length)
 {
-	if (midi_write_varlen((cycles - cycles_prev) >> 14))
+	unsigned long timestamp_delta = (cycles - cycles_prev) >> 14;
+	if (midi_write_varlen(timestamp_delta))
 		return 1;
 
-	if (fwrite(s, 1, n, file) != n)
+	if (midi_write_data(data, length))
 		return 1;
-
-	track_length += n;
 
 	cycles_prev = cycles;
 	return 0;
@@ -119,45 +123,45 @@ static int midi_open_track(int subsong)
 	free(filename);
 
 	/* Header type */
-	if (fwrite("MThd", 1, 4, file) != 4)
-		goto out_file;
+	if (midi_write_string("MThd"))
+		goto out;
 
 	/* Header length */
 	if (midi_write_u32(6))
-		goto out_file;
+		goto out;
 
 	/* Format */
 	if (midi_write_u16(0))
-		goto out_file;
+		goto out;
 
 	/* Tracks */
 	if (midi_write_u16(1))
-		goto out_file;
+		goto out;
 
 	/* Division */
-	/* XXX: Do some real calculation instead of this magic number :-) */
+	/* TODO: Do some real calculation instead of this magic number :-) */
 	if (midi_write_u16(124))
-		goto out_file;
+		goto out;
 
 	/* Track type */
-	if (fwrite("MTrk", 1, 4, file) != 4)
-		goto out_file;
+	if (midi_write_string("MTrk"))
+		goto out;
 
 	/* Track length (placeholder) */
 	track_length = 0;
 	track_length_offset = ftell(file);
 	if (track_length_offset == -1)
-		goto out_file;
+		goto out;
 
 	if (midi_write_u32(0))
-		goto out_file;
+		goto out;
 
 	return 0;
 
-out_file:
-	fclose(file);
-	file = NULL;
 out:
+	if (file != NULL)
+		fclose(file);
+	file = NULL;
 	return 1;
 }
 
@@ -271,13 +275,13 @@ int midi_skip(int subsong)
 
 void midi_close(void)
 {
-	int chan;
+	int channel;
 
 	if (!file)
 		return;
 
-	for (chan = 0; chan < 4; chan++)
-		if (midi_note_off(cycles_prev + 1, chan))
+	for (channel = 0; channel < 4; channel++)
+		if (midi_note_off(cycles_prev + 1, channel))
 			return;
 
 	midi_close_track();
