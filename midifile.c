mmitch/gbsplay

commit 1ab5960298e461500f00cd4601aad0a9c58e4585

refactor: extract common code from midi and altmidi plugouts
Create midifile.h and midifile.c containing the common basic code to
write a MIDI file.  The plugouts are reduced to the mapping between
GBS output and MIDI events.

midifile.c
@@ -1,0 +1,285 @@
+/*
+ * gbsplay is a Gameboy sound player
+ *
+ * common code of MIDI output plugins
+ *
+ * 2006-2022 (C) by Christian Garbs <mitch@cgarbs.de>
+ *                  Vegard Nossum
+ *
+ * Licensed under GNU GPL v1 or, at your option, any later version.
+ */
+
+#include "common.h"
+
+#include <stdlib.h>
+
+#include "plugout.h"
+
+#include "midifile.h"
+
+#define FILENAMESIZE 32
+
+static FILE *file = NULL;
+
+static long track_length;
+static long track_length_offset;
+static long mute[4] = {0, 0, 0, 0};
+
+int note[4];
+
+static cycles_t cycles_prev = 0;
+
+int midi_file_is_closed() {
+	return file == NULL;
+}
+
+void midi_update_mute(const struct gbs_channel_status status[]) {
+	for (int chan = 0; chan < 4; chan++)
+		mute[chan] = status[chan].mute;
+}
+
+static int midi_write_u32(uint32_t x)
+{
+	uint8_t s[4];
+
+	s[0] = x >> 24;
+	s[1] = x >> 16;
+	s[2] = x >> 8;
+	s[3] = x;
+
+	if (fwrite(s, 1, 4, file) != 4)
+		return 1;
+	return 0;
+}
+
+static int midi_write_u16(uint16_t x)
+{
+	uint8_t s[2];
+
+	s[0] = x >> 8;
+	s[1] = x;
+
+	if (fwrite(s, 1, 2, file) != 2)
+		return 1;
+	return 0;
+}
+
+static int midi_write_varlen(unsigned long x)
+{
+	uint8_t data[4];
+	unsigned int i;
+
+	for (i = 0; i < 4; ++i) {
+		data[3 - i] = (x & 0x7f) | 0x80;
+		x >>= 7;
+
+		if (!x) {
+			++i;
+			break;
+		}
+	}
+
+	data[3] &= ~0x80;
+	if (fwrite(data + 4 - i, 1, i, file) != i)
+		return 1;
+
+	track_length += i;
+	return 0;
+}
+
+static int midi_write_event(cycles_t cycles, const uint8_t *s, unsigned int n)
+{
+	if (midi_write_varlen((cycles - cycles_prev) >> 14))
+		return 1;
+
+	if (fwrite(s, 1, n, file) != n)
+		return 1;
+
+	track_length += n;
+
+	cycles_prev = cycles;
+	return 0;
+}
+
+static int midi_open_track(int subsong)
+{
+	char *filename;
+	if ((filename = malloc(FILENAMESIZE)) == NULL)
+		goto out;
+
+	if (snprintf(filename, FILENAMESIZE, "gbsplay-%d.mid", subsong + 1) >= FILENAMESIZE)
+		goto out;
+
+	file = fopen(filename, "wb");
+	if (!file) {
+		free(filename);
+		goto out;
+	}
+
+	free(filename);
+
+	/* Header type */
+	if (fwrite("MThd", 1, 4, file) != 4)
+		goto out_file;
+
+	/* Header length */
+	if (midi_write_u32(6))
+		goto out_file;
+
+	/* Format */
+	if (midi_write_u16(0))
+		goto out_file;
+
+	/* Tracks */
+	if (midi_write_u16(1))
+		goto out_file;
+
+	/* Division */
+	/* XXX: Do some real calculation instead of this magic number :-) */
+	if (midi_write_u16(124))
+		goto out_file;
+
+	/* Track type */
+	if (fwrite("MTrk", 1, 4, file) != 4)
+		goto out_file;
+
+	/* Track length (placeholder) */
+	track_length = 0;
+	track_length_offset = ftell(file);
+	if (track_length_offset == -1)
+		goto out_file;
+
+	if (midi_write_u32(0))
+		goto out_file;
+
+	return 0;
+
+out_file:
+	fclose(file);
+	file = NULL;
+out:
+	return 1;
+}
+
+static int midi_close_track()
+{
+	uint8_t event[3];
+
+	/* End of track */
+	event[0] = 0xff;
+	event[1] = 0x2f;
+	event[2] = 0x00;
+
+	if (midi_write_event(cycles_prev, event, 3))
+		goto out;
+
+	if (fseek(file, track_length_offset, SEEK_SET) == -1)
+		goto out;
+
+	if (midi_write_u32(track_length))
+		goto out;
+
+	fclose(file);
+	file = NULL;
+	return 0;
+
+out:
+	file = NULL;
+	return 1;
+}
+
+int midi_note_on(cycles_t cycles, int channel, int new_note, int velocity)
+{
+	uint8_t event[3];
+
+	if (mute[channel])
+		return 0;
+
+	event[0] = 0x90 | channel;
+	event[1] = new_note;
+	event[2] = velocity;
+
+	if (midi_write_event(cycles, event, 3))
+		return 1;
+
+	note[channel] = new_note;
+
+	return 0;
+}
+
+int midi_note_off(cycles_t cycles, int channel)
+{
+	uint8_t event[3];
+
+	if (!note[channel])
+		return 0;
+
+	event[0] = 0x80 | channel;
+	event[1] = note[channel];
+	event[2] = 0;
+
+	if (midi_write_event(cycles, event, 3))
+		return 1;
+
+	note[channel] = 0;
+
+	return 0;
+}
+
+int midi_pan(cycles_t cycles, int channel, int pan)
+{
+	uint8_t event[3];
+
+	if (mute[channel])
+		return 0;
+
+	event[0] = 0xb0 | channel;
+	event[1] = 0x0a;
+	event[2] = pan;
+
+	if (midi_write_event(cycles, event, 3))
+		return 1;
+
+	return 0;
+}
+
+long midi_open(enum plugout_endian endian, long rate, long *buffer_bytes)
+{
+	UNUSED(endian);
+	UNUSED(rate);
+	UNUSED(buffer_bytes);
+
+	return 0;
+}
+
+int midi_skip(int subsong)
+{
+	int channel;
+
+	if (file) {
+		if (midi_close_track())
+			return 1;
+	}
+
+	cycles_prev = 0;
+
+	for (channel = 0; channel < 4; channel++)
+		note[channel] = 0;
+
+	return midi_open_track(subsong);
+}
+
+void midi_close(void)
+{
+	int chan;
+
+	if (!file)
+		return;
+
+	for (chan = 0; chan < 4; chan++)
+		if (midi_note_off(cycles_prev + 1, chan))
+			return;
+
+	midi_close_track();
+}
+

Makefile
@@ -140,10 +140,10 @@ ifeq ($(plugout_stdout),yes)
 plugout_objs +=plugout_stdout.o
 endif
 ifeq ($(plugout_midi),yes)
-plugout_objs +=plugout_midi.o
+plugout_objs +=plugout_midi.o midifile.o
 endif
 ifeq ($(plugout_altmidi),yes)
-plugout_objs +=plugout_altmidi.o
+plugout_objs +=plugout_altmidi.o midifile.o
 endif
 ifeq ($(plugout_pulse),yes)
 plugout_objs +=plugout_pulse.o
@@ -159,6 +159,9 @@ endif
 ifeq ($(plugout_wav),yes)
 plugout_objs +=plugout_wav.o
 endif
+
+# dedupe (and finalize) plugout_objs; order is irrelevant, sorting is ok
+plugout_objs := $(sort $(plugout_objs))
 
 objs_gbsplay +=$(plugout_objs)
 objs_xgbsplay += $(plugout_objs)

midifile.h
@@ -1,0 +1,31 @@
+/*
+ * gbsplay is a Gameboy sound player
+ *
+ * common code of MIDI output plugins
+ *
+ * 2022 (C) by Christian Garbs <mitch@cgarbs.de>
+ *
+ * Licensed under GNU GPL v1 or, at your option, any later version.
+ */
+
+#ifndef _MIDIFILE_H_
+#define _MIDIFILE_H_
+
+#include "common.h"
+
+#include "plugout.h"
+
+extern int note[4];
+
+extern int  midi_file_is_closed();
+extern void midi_update_mute(const struct gbs_channel_status status[]);
+
+extern int  midi_note_on(cycles_t cycles, int channel, int new_note, int velocity);
+extern int  midi_note_off(cycles_t cycles, int channel);
+extern int  midi_pan(cycles_t cycles, int channel, int pan);
+
+extern long midi_open(enum plugout_endian endian, long rate, long *buffer_bytes);
+extern int  midi_skip(int subsong);
+extern void midi_close(void);
+
+#endif

plugout_altmidi.c
@@ -12,276 +12,39 @@
 #include "common.h"
 
 #include <math.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
+#include "midifile.h"
 #include "plugout.h"
-#include "gbhw.h"
 
-#define FILENAMESIZE 32
-
-static long midi_open(enum plugout_endian endian, long rate, long *buffer_bytes)
-{
-	return 0;
-}
-
-static FILE *file = NULL;
-
-static long track_length;
-static long track_length_offset;
-
-static cycles_t cycles_prev = 0;
-
-static int note[4] = {0, 0, 0, 0};
-static int volume[4] = {0, 0, 0, 0};
+static int volume[4]  = {0, 0, 0, 0};
 static int playing[4] = {0, 0, 0, 0};
-static long mute[4] = {0, 0, 0, 0};
 
-static int midi_write_u32(uint32_t x)
-{
-	uint8_t s[4];
-
-	s[0] = x >> 24;
-	s[1] = x >> 16;
-	s[2] = x >> 8;
-	s[3] = x;
-
-	if (fwrite(s, 1, 4, file) != 4)
-		return 1;
-	return 0;
-}
-
-static int midi_write_u16(uint16_t x)
-{
-	uint8_t s[2];
-
-	s[0] = x >> 8;
-	s[1] = x;
-
-	if (fwrite(s, 1, 2, file) != 2)
-		return 1;
-	return 0;
-}
-
-static int midi_write_varlen(unsigned long x)
-{
-	uint8_t data[4];
-	unsigned int i;
-
-	for (i = 0; i < 4; ++i) {
-		data[3 - i] = (x & 0x7f) | 0x80;
-		x >>= 7;
-
-		if (!x) {
-			++i;
-			break;
-		}
-	}
-
-	data[3] &= ~0x80;
-	if (fwrite(data + 4 - i, 1, i, file) != i)
-		return 1;
-
-	track_length += i;
-	return 0;
-}
-
-static int midi_write_event(cycles_t cycles, const uint8_t *s, unsigned int n)
-{
-	if (midi_write_varlen((cycles - cycles_prev) >> 14))
-		return 1;
-
-	if (fwrite(s, 1, n, file) != n)
-		return 1;
-
-	track_length += n;
-
-	cycles_prev = cycles;
-	return 0;
-}
-
-static int midi_open_track(int subsong)
-{
-	char *filename;
-	if ((filename = malloc(FILENAMESIZE)) == NULL)
-		goto out;
-	
-	if (snprintf(filename, FILENAMESIZE, "gbsplay-%d.mid", subsong + 1) >= FILENAMESIZE)
-		goto out;
-
-	file = fopen(filename, "wb");
-	if (!file) {
-		free(filename);
-		goto out;
-	}
-
-	free(filename);
-
-	/* Header type */
-	if (fwrite("MThd", 1, 4, file) != 4)
-		goto out_file;
-
-	/* Header length */
-	if (midi_write_u32(6))
-		goto out_file;
-
-	/* Format */
-	if (midi_write_u16(0))
-		goto out_file;
-
-	/* Tracks */
-	if (midi_write_u16(1))
-		goto out_file;
-
-	/* Division */
-	/* XXX: Do some real calculation instead of this magic number :-) */
-	if (midi_write_u16(124))
-		goto out_file;
-
-	/* Track type */
-	if (fwrite("MTrk", 1, 4, file) != 4)
-		goto out_file;
-
-	/* Track length (placeholder) */
-	track_length = 0;
-	track_length_offset = ftell(file);
-	if (track_length_offset == -1)
-		goto out_file;
-
-	if (midi_write_u32(0))
-		goto out_file;
-
-	return 0;
-
-out_file:
-	fclose(file);
-	file = NULL;
-out:
-	return 1;
-}
-
-static int midi_close_track()
-{
-	uint8_t event[3];
-
-	/* End of track */
-	event[0] = 0xff;
-	event[1] = 0x2f;
-	event[2] = 0x00;
-
-	if (midi_write_event(cycles_prev, event, 3))
-		goto out;
-
-	if (fseek(file, track_length_offset, SEEK_SET) == -1)
-		goto out;
-
-	if (midi_write_u32(track_length))
-		goto out;
-
-	fclose(file);
-	file = NULL;
-	return 0;
-
-out:
-	file = NULL;
-	return 1;
-}
-
-static int midi_skip(int subsong)
-{
-	cycles_prev = 0;
-
-	if (file) {
-		if (midi_close_track())
-			return 1;
-	}
-
-	return midi_open_track(subsong);
-}
-
-static int note_on(cycles_t cycles, int channel, int new_note)
-{
-	uint8_t event[3];
-
-	if (mute[channel])
-		return 0;
-
-	event[0] = 0x90 | channel;
-	event[1] = new_note;
-	event[2] = volume[channel];
-
-	if (midi_write_event(cycles, event, 3))
-		return 1;
-
-	note[channel] = new_note;
-
-	return 0;
-}
-
-static int note_off(cycles_t cycles, int channel)
-{
-	uint8_t event[3];
-
-	if (!note[channel])
-		return 0;
-
-	event[0] = 0x80 | channel;
-	event[1] = note[channel];
-	event[2] = 0;
-
-	if (midi_write_event(cycles, event, 3))
-		return 1;
-
-	note[channel] = 0;
-	
-	return 0;
-}
-
-static int pan(cycles_t cycles, int channel, int pan)
-{
-	uint8_t event[3];
-
-	if (mute[channel])
-		return 0;
-
-	event[0] = 0xb0 | channel;
-	event[1] = 0x0a;
-	event[2] = pan;
-
-	if (midi_write_event(cycles, event, 3))
-		return 1;
-
-	return 0;
-}
-
-static int midi_step(cycles_t cycles, const struct gbs_channel_status chan[])
+static int midi_step(cycles_t cycles, const struct gbs_channel_status status[])
 {
 	int c;
 	int new_playing;
 	int new_note;
 	const struct gbs_channel_status *ch;
 
+	midi_update_mute(status);
+	
 	for (c = 0; c < 3; c++) {
-		ch = &chan[c];
+		ch = &status[c];
 		new_playing = ch->playing;
-		mute[c] = ch->mute;
 
 		if (playing[c]) {
 			if (new_playing) {
 				new_note = NOTE(ch->div_tc) + 21;
 				if (new_note != note[c]) {
-					if (note_off(cycles, c))
+					if (midi_note_off(cycles, c))
 						return 1;
 					if (new_note < 0 || new_note >= 0x80)
 						continue;
-					if (note_on(cycles, c, new_note))
+					if (midi_note_on(cycles, c, new_note, volume[c]))
 						return 1;
 				}
 			} else {
-				if (note_off(cycles, c))
+				if (midi_note_off(cycles, c))
 					return 1;
 				playing[c] = 0;
 			}
@@ -290,7 +53,7 @@ static int midi_step(cycles_t cycles, const struct gbs_channel_status chan[])
 				new_note = NOTE(ch->div_tc) + 21;
 				if (new_note < 0 || new_note >= 0x80)
 					continue;
-				if (note_on(cycles, c, new_note))
+				if (midi_note_on(cycles, c, new_note, volume[c]))
 					return 1;
 				playing[c] = 1;
 			}
@@ -305,7 +68,7 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 {
 	long chan = (addr - 0xff10) / 5;
 
-	if (!file)
+	if (midi_file_is_closed())
 		return 1;
 
 	switch (addr) {
@@ -319,7 +82,7 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 	case 0xff1e:
 		/* Channel start trigger */
 		if ((val & 0x80) == 0x80) {
-			if (note_off(cycles, chan))
+			if (midi_note_off(cycles, chan))
 				return 1;
 			playing[chan] = 0;
 		}
@@ -333,33 +96,19 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 		for (chan = 0; chan < 4; chan++)
 			switch ((val >> chan) & 0x11) {
 			case 0x10:
-				pan(cycles, chan, 0);
+				midi_pan(cycles, chan, 0);
 				break;
 			case 0x01:
-				pan(cycles, chan, 127);
+				midi_pan(cycles, chan, 127);
 				break;
 			default:
-				pan(cycles, chan, 64);
+				midi_pan(cycles, chan, 64);
 			}
 		break;
 		
 	}
 
 	return 0;
-}
-
-static void midi_close(void)
-{
-	int chan;
-	
-	if (!file)
-		return;
-
-	for (chan = 0; chan < 4; chan++)
-		if (note_off(cycles_prev + 1, chan))
-			return;
-	
-	midi_close_track();
 }
 
 const struct output_plugin plugout_altmidi = {

plugout_midi.c
@@ -12,251 +12,9 @@
 #include "common.h"
 
 #include <math.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
+#include "midifile.h"
 #include "plugout.h"
-
-#define FILENAMESIZE 32
-
-static long midi_open(enum plugout_endian endian, long rate, long *buffer_bytes)
-{
-	UNUSED(endian);
-	UNUSED(rate);
-	UNUSED(buffer_bytes);
-
-	return 0;
-}
-
-static FILE *file = NULL;
-
-static long track_length;
-static long track_length_offset;
-
-static cycles_t cycles_prev = 0;
-
-static int note[4] = {0, 0, 0, 0};
-static long mute[4] = {0, 0, 0, 0};
-
-static int midi_write_u32(uint32_t x)
-{
-	uint8_t s[4];
-
-	s[0] = x >> 24;
-	s[1] = x >> 16;
-	s[2] = x >> 8;
-	s[3] = x;
-
-	if (fwrite(s, 1, 4, file) != 4)
-		return 1;
-	return 0;
-}
-
-static int midi_write_u16(uint16_t x)
-{
-	uint8_t s[2];
-
-	s[0] = x >> 8;
-	s[1] = x;
-
-	if (fwrite(s, 1, 2, file) != 2)
-		return 1;
-	return 0;
-}
-
-static int midi_write_varlen(unsigned long x)
-{
-	uint8_t data[4];
-	unsigned int i;
-
-	for (i = 0; i < 4; ++i) {
-		data[3 - i] = (x & 0x7f) | 0x80;
-		x >>= 7;
-
-		if (!x) {
-			++i;
-			break;
-		}
-	}
-
-	data[3] &= ~0x80;
-	if (fwrite(data + 4 - i, 1, i, file) != i)
-		return 1;
-
-	track_length += i;
-	return 0;
-}
-
-static int midi_write_event(cycles_t cycles, const uint8_t *s, unsigned int n)
-{
-	if (midi_write_varlen((cycles - cycles_prev) >> 14))
-		return 1;
-
-	if (fwrite(s, 1, n, file) != n)
-		return 1;
-
-	track_length += n;
-
-	cycles_prev = cycles;
-	return 0;
-}
-
-static int midi_open_track(int subsong)
-{
-	char *filename;
-	if ((filename = malloc(FILENAMESIZE)) == NULL)
-		goto out;
-	
-	if (snprintf(filename, FILENAMESIZE, "gbsplay-%d.mid", subsong + 1) >= FILENAMESIZE)
-		goto out;
-
-	file = fopen(filename, "wb");
-	if (!file) {
-		free(filename);
-		goto out;
-	}
-
-	free(filename);
-
-	/* Header type */
-	if (fwrite("MThd", 1, 4, file) != 4)
-		goto out_file;
-
-	/* Header length */
-	if (midi_write_u32(6))
-		goto out_file;
-
-	/* Format */
-	if (midi_write_u16(0))
-		goto out_file;
-
-	/* Tracks */
-	if (midi_write_u16(1))
-		goto out_file;
-
-	/* Division */
-	/* XXX: Do some real calculation instead of this magic number :-) */
-	if (midi_write_u16(124))
-		goto out_file;
-
-	/* Track type */
-	if (fwrite("MTrk", 1, 4, file) != 4)
-		goto out_file;
-
-	/* Track length (placeholder) */
-	track_length = 0;
-	track_length_offset = ftell(file);
-	if (track_length_offset == -1)
-		goto out_file;
-
-	if (midi_write_u32(0))
-		goto out_file;
-
-	return 0;
-
-out_file:
-	fclose(file);
-	file = NULL;
-out:
-	return 1;
-}
-
-static int midi_close_track()
-{
-	uint8_t event[3];
-
-	/* End of track */
-	event[0] = 0xff;
-	event[1] = 0x2f;
-	event[2] = 0x00;
-
-	if (midi_write_event(cycles_prev, event, 3))
-		goto out;
-
-	if (fseek(file, track_length_offset, SEEK_SET) == -1)
-		goto out;
-
-	if (midi_write_u32(track_length))
-		goto out;
-
-	fclose(file);
-	file = NULL;
-	return 0;
-
-out:
-	file = NULL;
-	return 1;
-}
-
-static int midi_skip(int subsong)
-{
-	cycles_prev = 0;
-
-	if (file) {
-		if (midi_close_track())
-			return 1;
-	}
-
-	return midi_open_track(subsong);
-}
-
-static int note_on(cycles_t cycles, int channel, int new_note, int velocity)
-{
-	uint8_t event[3];
-
-	if (mute[channel])
-		return 0;
-
-	event[0] = 0x90 | channel;
-	event[1] = new_note;
-	event[2] = velocity;
-
-	if (midi_write_event(cycles, event, 3))
-		return 1;
-
-	note[channel] = new_note;
-
-	return 0;
-}
-
-static int note_off(cycles_t cycles, int channel)
-{
-	uint8_t event[3];
-
-	if (!note[channel])
-		return 0;
-
-	event[0] = 0x80 | channel;
-	event[1] = note[channel];
-	event[2] = 0;
-
-	if (midi_write_event(cycles, event, 3))
-		return 1;
-
-	note[channel] = 0;
-	
-	return 0;
-}
-
-static int pan(cycles_t cycles, int channel, int pan)
-{
-	uint8_t event[3];
-
-	if (mute[channel])
-		return 0;
-
-	event[0] = 0xb0 | channel;
-	event[1] = 0x0a;
-	event[2] = pan;
-
-	if (midi_write_event(cycles, event, 3))
-		return 1;
-
-	return 0;
-}
 
 static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 {
@@ -268,7 +26,7 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 
 	long chan = (addr - 0xff10) / 5;
 
-	if (!file)
+	if (midi_file_is_closed())
 		return 1;
 
 	switch (addr) {
@@ -278,7 +36,7 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 		master[chan] = (val & 0xf8) != 0;
 		if (!master[chan] && running[chan]) {
 			/* DAC turned off, disable channel */
-			if (note_off(cycles, chan))
+			if (midi_note_off(cycles, chan))
 				return 1;
 			running[chan] = 0;
 		}
@@ -288,12 +46,12 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 				new_note = NOTE(2048 - div[chan]) + 21;
 				if (new_note < 0 || new_note >= 0x80)
 					break;
-				if (note_on(cycles, chan, new_note, volume[chan]))
+				if (midi_note_on(cycles, chan, new_note, volume[chan]))
 					return 1;
 			}
 		} else {
 			/* volume set to 0, stop note (if any) */
-			if (note_off(cycles, chan))
+			if (midi_note_off(cycles, chan))
 				return 1;
 		}
 		break;
@@ -308,13 +66,13 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 
 			if (new_note != note[chan]) {
 				/* portamento: retrigger with new note */
-				if (note_off(cycles, chan))
+				if (midi_note_off(cycles, chan))
 					return 1;
 
 				if (new_note < 0 || new_note >= 0x80)
 					break;
 
-				if (note_on(cycles, chan, new_note, volume[chan]))
+				if (midi_note_on(cycles, chan, new_note, volume[chan]))
 					return 1;
 			}
 		}
@@ -330,14 +88,14 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 
 		/* Channel start trigger */
 		if ((val & 0x80) == 0x80) {
-			if (note_off(cycles, chan))
+			if (midi_note_off(cycles, chan))
 				return 1;
 
 			if (new_note < 0 || new_note >= 0x80)
 				break;
 
 			if (master[chan]) {
-				if (note_on(cycles, chan, new_note, volume[chan]))
+				if (midi_note_on(cycles, chan, new_note, volume[chan]))
 					return 1;
 				running[chan] = 1;
 			}
@@ -345,13 +103,13 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 			if (running[chan]) {
 				if (new_note != note[chan]) {
 					/* portamento: retrigger with new note */
-					if (note_off(cycles, chan))
+					if (midi_note_off(cycles, chan))
 						return 1;
 
 					if (new_note < 0 || new_note >= 0x80)
 						break;
 
-					if (note_on(cycles, chan, new_note, volume[chan]))
+					if (midi_note_on(cycles, chan, new_note, volume[chan]))
 						return 1;
 				}
 			}
@@ -362,7 +120,7 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 		master[2] = (val & 0x80) == 0x80;
 		if (!master[2] && running[2]) {
 			/* DAC turned off, disable channel */
-			if (note_off(cycles, 2))
+			if (midi_note_off(cycles, 2))
 				return 1;
 			running[2] = 0;
 		}
@@ -375,12 +133,12 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 				new_note = NOTE(2048 - div[chan]) + 21;
 				if (new_note < 0 || new_note >= 0x80)
 					break;
-				if (note_on(cycles, 2, new_note, volume[2]))
+				if (midi_note_on(cycles, 2, new_note, volume[2]))
 					return 1;
 			}
 		} else {
 			/* volume set to 0, stop note (if any) */
-			if (note_off(cycles, 2))
+			if (midi_note_off(cycles, 2))
 				return 1;
 		}
 		break;
@@ -388,13 +146,13 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 		for (chan = 0; chan < 4; chan++)
 			switch ((val >> chan) & 0x11) {
 			case 0x10:
-				pan(cycles, chan, 0);
+				midi_pan(cycles, chan, 0);
 				break;
 			case 0x01:
-				pan(cycles, chan, 127);
+				midi_pan(cycles, chan, 127);
 				break;
 			default:
-				pan(cycles, chan, 64);
+				midi_pan(cycles, chan, 64);
 			}
 		break;
 	case 0xff26:
@@ -404,7 +162,7 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 				volume[chan] = 0;
 				running[chan] = 0;
 				master[chan] = 1;
-				if (note_off(cycles, 2))
+				if (midi_note_off(cycles, 2))
 					return 1;
 			}
 		}
@@ -415,28 +173,11 @@ static int midi_io(cycles_t cycles, uint32_t addr, uint8_t val)
 }
 
 static int midi_step(cycles_t cycles, const struct gbs_channel_status status[]) {
-	int chan;
-
 	UNUSED(cycles);
 
-	for (chan = 0; chan < 4; chan++)
-		mute[chan] = status[chan].mute;
+	midi_update_mute(status);
 
 	return 0;
-}
-
-static void midi_close(void)
-{
-	int chan;
-	
-	if (!file)
-		return;
-
-	for (chan = 0; chan < 4; chan++)
-		if (note_off(cycles_prev + 1, chan))
-			return;
-	
-	midi_close_track();
 }
 
 const struct output_plugin plugout_midi = {

commit c202e1afb56587ba15e564a07824290e4edbad92

fix midi and altmidi plugouts
Header length calculation was broken starting with
commit 1ab5960298e461500f00cd4601aad0a9c58e4585
because it was written with wrong endianess.

We need some simple tests like we have for the wav plugout.

midifile.c
@@ -144,7 +144,7 @@ static int midi_close_track()
 		goto error;
 
 	track_length = track_end_offset - track_start_offset;
-	if (file_write_32bit_le_at(file, track_length, track_length_offset) == -1)
+	if (file_write_32bit_be_at(file, track_length, track_length_offset) == -1)
 		goto error;
 
 	/* Close the file */